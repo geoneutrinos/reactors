ce69ecf61585b415726fa135a3d42dce26537425
Andrew Barna <abarna@gmail.com>
2019-11-09 08:03:03 -1000

    add crust model data

---
+++ src/crust-model/README.md
@@ -0,0 +1,9 @@
+Crust Model
+===========
+This module has the lookup table for unocilated crust neutrino fluxes from Huang et. al. 2013 10.1002/ggge.20129
+
+Files
+-----
+* Huang_crust_flux.xlsx - Origional Data
+* huang_crust_to_json.py - generates the json documents for the various elements
+* crust_{element}.json - files made by the above python script containing the data for the columns we want
---
+++ src/crust-model/huang_crust_to_json.py
@@ -0,0 +1,39 @@
+import json
+
+import pyexcel
+import pyexcel_xlsx
+
+import logging
+logging.basicConfig(level=logging.INFO)
+
+raw_data = pyexcel.get_array(file_name="Huang_crust_flux.xlsx")
+
+
+data = {
+    "u": [],
+    "th": [],
+    "k": [],
+}
+
+U_COL = 2
+TH_COL = 3
+K_COL = 4
+
+# The first two rows are header info, so just start at the data
+for row in raw_data[2:]:
+    if row[0] == "":
+        # This should be the end of the data, to just stop
+        break
+    data["u"].append(float(row[U_COL]))
+    data["th"].append(float(row[TH_COL]))
+    data["k"].append(float(row[K_COL]))
+
+# the input data is on a 1x1 degree grid of lon lats
+expected_array_length = 360 * 180
+assert(len(data["u"]) == expected_array_length)
+assert(len(data["th"]) == expected_array_length)
+assert(len(data["k"]) == expected_array_length)
+
+for element, flux in data.items():
+    with open(f"crust_{element}.json", "w") as f:
+        json.dump(flux, f)
