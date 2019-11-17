802ef0bc1b8ff88180a29e439d57f59811aceac3
Andrew Barna <abarna@gmail.com>
2019-11-16 16:53:55 -0800

    try some memoizing

--- src/App.js
+++ src/App.js
@@ -1,29 +1,42 @@
 import React from 'react';
 import { getCrustFlux } from './crust-model'
+import { normalNeutrinoOscilationSpectrum, invertedNeutrinoOscilationSpectrum } from './physics/neutrino-oscillation'
+import {memoize} from 'lodash';
+
+let memoed_nuosc = memoize(normalNeutrinoOscilationSpectrum)
+let memoedi_nuosc = memoize(invertedNeutrinoOscilationSpectrum)
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state ={
-      crustFlux: ""
+      crustFlux: "",
+      normal: "",
+      inverted: ""
     }
   }
   componentDidMount = () => {
     setInterval(() => {
       const lat = Math.random() * 180 - 90;
       const lon = Math.random() * 360 - 180;
+      const dist = Math.floor(Math.random() * 6000);
       const crustFlux = JSON.stringify(getCrustFlux(lon,lat));
+      const normal= JSON.stringify(memoed_nuosc(dist)[0]);
+      const invert = JSON.stringify(memoedi_nuosc(dist)[0]);
       if (crustFlux === '{}'){
         console.log(lon, lat)
       }
-      this.setState({crustFlux: crustFlux});
+      this.setState({crustFlux: crustFlux, normal: normal, invert: invert});
     }, 1)
   }
 
   render() {
     return (
       <div className="App">
-        {this.state.crustFlux}
+        {this.state.crustFlux}<br/>
+        {this.state.normal}<br/>
+        {this.state.invert}<br/>
+        {this.state.normal - this.state.invert}<br/>
       </div>
     );
   }
