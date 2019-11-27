d5cb9753002e04510305b9f9c5c6d96b94dd1e47
Andrew Barna <abarna@gmail.com>
2019-11-27 13:32:52 -0700

    Cores: work in progress porting

--- src/reactor-cores/index.ts
+++ src/reactor-cores/index.ts
@@ -1,3 +1,27 @@
-import {reactors as cores, times, loads} from './reactor-database/reactors.json'
+import { reactors as cores, times, loads } from './reactor-database/reactors.json'
+import { range } from 'lodash';
+import { project } from 'ecef-projector';
 
-export {cores, times, loads};
+export { cores, times, loads };
+
+console.log(cores, times, loads)
+
+function mevRange(count = 1000, start = 0, stop = 10){
+  // TODO figure out how to deal with a start not a zero
+  const binSize = (stop - start) / count;
+  return new Float32Array(range(binSize/2, stop, binSize))
+}
+
+class LoadFactor {
+    date: Date;
+    load: number;
+    days: number;
+
+    constructor({ date, load }: { date: string; load: number; }) {
+        this.date = new Date(date);
+        this.load = load / 100;
+        // This is finding the "zeroith" day of the next month, which will result
+        // in the last day of the month we want being returned
+        this.days = new Date(this.date.getFullYear(), this.date.getMonth() + 1, 0).getDate();
+    }
+}
