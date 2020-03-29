090100e363e6ef4637932fb0c143fbfc02a830c0
Andrew Barna <abarna@gmail.com>
2020-03-29 23:26:49 +0000

    Provide full precision numbers as titles

--- src/ui/detector.jsx
+++ src/ui/detector.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import {sum} from 'lodash'
 
+function Num({v, p}){
+    return <span title={v}>{v.toFixed(p)}</span>
+}
+
 export function StatsPanel({cores, spectrum}) {
     const unit = "NIU";
 
@@ -40,7 +44,7 @@ export function StatsPanel({cores, spectrum}) {
             <i>D</i><sub>closest</sub> = {closestDistace > 100000? "N/A": closestDistace.toFixed(2)} km<br />
             <span style={{ "display": customDisplay}}><i>D</i><sub>user</sub> = {customClosestDistance.toFixed(3)} km<br /></span>
             <span style={{ "display": customDisplay}}><i>R</i><sub>user</sub> = {customClosestNIU.toFixed(1)} {unit}<br /></span>
-            <i>R</i><sub>geo</sub> = {geoTotalNIU.toFixed(1)} {unit} (U = {geoUNIU.toFixed(1)}, Th = {geoThNIU.toFixed(1)}, K = {geoKNIU.toFixed(1)})<br />
+            <i>R</i><sub>geo</sub> = <Num v={geoTotalNIU} p={1}/> {unit} (U = <Num v={geoUNIU} p={1}/>, Th = <Num v={geoThNIU} p={1}/>, K = <Num v={geoKNIU} p={1}/>)<br />
             <small>NIU = Neutrino interaction unit</small><br />
             <small>1 {unit} = 1 interaction/10<sup>32</sup> targets/year</small><br />
             <small>1 kT H<sub>2</sub>O contains 0.668559x10<sup>32</sup> free protons</small>
