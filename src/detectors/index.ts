77409efc5edfdd7b339aa3d5cf6d6ab26daa405e
Andrew Barna <abarna@gmail.com>
2020-06-20 00:52:41 +0000

    Add elevations to detector presets

--- src/detectors/index.ts
+++ src/detectors/index.ts
@@ -13,35 +13,35 @@ export const presets: DetecorPreset[] = [
     {name: "Guemseong", lat:35.05, lon: 126.70, elevation:0, overburden:950, region:"Asia"},
     {name: "INO", lat:9.95, lon: 77.28, elevation:0, overburden:3000, region:"Asia"},
     {name: "Jiangmen", lat:22.12, lon: 112.51, elevation:-481.25, overburden:2100, region:"Asia"},
-    {name: "Jinping", lat:28.15, lon: 101.71, elevation:0, overburden:6720, region:"Asia"},
+    {name: "Jinping", lat:28.15, lon: 101.71, elevation:1500, overburden:6720, region:"Asia"},
     {name: "Kamioka", lat:36.41, lon: 137.30, elevation:368, overburden:2050, region:"Asia"},
-    {name: "Lake Baikal", lat:51.771, lon: 104.398, elevation:0, overburden:1100, region:"Asia"},
-    {name: "Yemilab", lat:37.188639, lon: 128.659406, elevation:0, overburden:2500, region:"Asia"},
+    {name: "Lake Baikal", lat:51.771, lon: 104.398, elevation:-644.5, overburden:1100, region:"Asia"},
+    {name: "Yemilab", lat:37.188639, lon: 128.659406, elevation:-35, overburden:2500, region:"Asia"},
     //Europe
-    {name: "Baksan", lat:43.24, lon: 42.70, elevation:0, overburden:4900, region:"Europe"},
+    {name: "Baksan", lat:43.24, lon: 42.70, elevation:1700, overburden:4900, region:"Europe"},
     {name: "Boulby", lat:54.555129, lon: -0.80089, elevation:-1050, overburden:2805, region:"Europe"},
-    {name: "Canfranc", lat:42.77, lon: -0.57, elevation:0, overburden:2450, region:"Europe"},
+    {name: "Canfranc", lat:42.77, lon: -0.57, elevation:1080, overburden:2450, region:"Europe"},
     {name: "Fréjus", lat:45.14, lon: 6.69, elevation:0, overburden:4200, region:"Europe"},
     {name: "LNGS", lat:42.45, lon: 13.58, elevation:936.45, overburden:3100, region:"Europe"},
     {name: "Pyhäsalmi", lat:63.66, lon: 26.04, elevation:0, overburden:4000, region:"Europe"},
     // Mediterranean Sea
-    {name: "Antares", lat:42.80, lon: 6.17, elevation:0, overburden:2500, region:"Mediterranean Sea"},
-    {name: "Nestor", lat:36.63, lon: 21.58, elevation:0, overburden:4000, region:"Mediterranean Sea"},
-    {name: "NEMO Test", lat:37.551, lon: 15.384, elevation:0, overburden:2080, region:"Mediterranean Sea"},
+    {name: "Antares", lat:42.80, lon: 6.17, elevation:-2500, overburden:2500, region:"Mediterranean Sea"},
+    {name: "Nestor", lat:36.63, lon: 21.58, elevation:-4000, overburden:4000, region:"Mediterranean Sea"},
+    {name: "NEMO Test", lat:37.551, lon: 15.384, elevation:2080, overburden:2080, region:"Mediterranean Sea"},
     // North America
     {name: "IMB", lat:41.75, lon: -81.29, elevation:0, overburden:1570, region:"North America"},
     {name: "KURF", lat:37.38, lon: -80.66, elevation:0, overburden:1400, region:"North America"},
     {name: "Soudan", lat:47.82, lon: -92.24, elevation:0, overburden:1950, region:"North America"},
-    {name: "SURF", lat:44.36, lon: -103.76, elevation:0, overburden:4300, region:"North America"},
-    {name: "WIPP", lat:32.37, lon: -103.79, elevation:0, overburden:1600, region:"North America"},
+    {name: "SURF", lat:44.36, lon: -103.76, elevation:192, overburden:4300, region:"North America"},
+    {name: "WIPP", lat:32.37, lon: -103.79, elevation:1039, overburden:1600, region:"North America"},
     {name: "SNOLAB", lat:46.47, lon: -81.20, elevation:309, overburden:6010, region:"North America"},
     // Oceania
-    {name: "SUPL", lat:-37.07, lon: 142.81, elevation:0, overburden:2700, region:"Oceania"},
+    {name: "SUPL", lat:-37.07, lon: 142.81, elevation:-794, overburden:2700, region:"Oceania"},
     // Pacific Ocean
-    {name: "ACO", lat:22.75, lon: -158, elevation:0, overburden:4800, region:"Pacific Ocean"},
-    {name: "MARS", lat:36.71, lon: -122.19, elevation:0, overburden:890, region:"Pacific Ocean"},
+    {name: "ACO", lat:22.75, lon: -158, elevation:-4800, overburden:4800, region:"Pacific Ocean"},
+    {name: "MARS", lat:36.71, lon: -122.19, elevation:-890, overburden:890, region:"Pacific Ocean"},
     // South America
-    {name: "ANDES", lat:-30.25, lon: -69.88, elevation:0, overburden:4200, region:"South America"},
+    {name: "ANDES", lat:-30.25, lon: -69.88, elevation:3800, overburden:4200, region:"South America"},
 ]
 
 interface Detector {
