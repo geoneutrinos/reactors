8af74d08bcb1b4fc015b37a454fb7ae7c13cdd21
Andrew Barna <abarna@gmail.com>
2020-06-28 11:18:22 -1000

    Physics Tab: swap mass order and cross section selects

--- src/ui/detector-physics.jsx
+++ src/ui/detector-physics.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Card, Form } from "react-bootstrap";
 
-import { MassOrdering } from '../physics/neutrino-oscillation'
+import { MassOrdering } from "../physics/neutrino-oscillation";
 
 export const DetectorPhysicsPane = ({
   massOrdering,
@@ -10,32 +10,48 @@ export const DetectorPhysicsPane = ({
   setCrossSection,
   XSNames,
 }) => {
+  const CrossSectionInput = (
+    <Form.Group controlId="neutrinoCrossSection">
+      <Form.Label>Neutrino Cross Section</Form.Label>
+      <Form.Control
+        as="select"
+        onChange={(event) => setCrossSection(event.target.value)}
+        value={crossSection}
+      >
+        {Object.values(XSNames).map((name) => {
+          return (
+            <option key={name} value={name}>
+              {name}
+            </option>
+          );
+        })}
+      </Form.Control>
+    </Form.Group>
+  );
+
+  const MassOrderingInput = (
+    <Form.Group controlId="neutrinoMassOrder">
+      <Form.Label>Neutrino Mass Ordering</Form.Label>
+      <Form.Control
+        as="select"
+        onChange={(event) => setMassOrdering(event.target.value)}
+        value={massOrdering}
+      >
+        {Object.values(MassOrdering).map((order) => (
+          <option key={order} value={order}>
+            {order}
+          </option>
+        ))}
+      </Form.Control>
+    </Form.Group>
+  );
+
   return (
     <Card>
       <Card.Body>
         <Card.Title>Physics</Card.Title>
-        <Form.Group controlId="neutrinoMassOrder">
-          <Form.Label>Neutrino Mass Ordering</Form.Label>
-          <Form.Control
-            as="select"
-            onChange={(event) => setMassOrdering(event.target.value)}
-            value={massOrdering}
-          >
-            {Object.values(MassOrdering).map( order => <option key={order} value={order}>{order}</option>)}
-          </Form.Control>
-        </Form.Group>
-        <Form.Group controlId="neutrinoCrossSection">
-          <Form.Label>Neutrino Cross Section</Form.Label>
-          <Form.Control
-            as="select"
-            onChange={(event) => setCrossSection(event.target.value)}
-            value={crossSection}
-          >
-            {Object.values(XSNames).map( name => {
-            return <option key={name} value={name}>{name}</option>
-            })}
-          </Form.Control>
-        </Form.Group>
+        {CrossSectionInput}
+        {MassOrderingInput}
       </Card.Body>
     </Card>
   );
