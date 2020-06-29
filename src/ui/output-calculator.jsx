50948993429fa1da5f214d6da21abaaf37818179
Andrew Barna <abarna@gmail.com>
2020-06-29 09:47:39 -1000

    Fix crash when adding a custom core

--- src/ui/output-calculator.jsx
+++ src/ui/output-calculator.jsx
@@ -93,7 +93,7 @@ export const CalculatorPanel = ({ cores, spectrum }) => {
   // custom cores
   const customCores = coreList.filter((core) => core.custom);
   const customTotalSignal = sum(
-    customCores.map((core) => core.detectorNIU.slice(min_i, max_i))
+    customCores.map((core) => core.detectorSignal.slice(min_i, max_i))
   );
 
   const geoUNIU = sum(spectrum.geoU.slice(min_i, max_i)) * 0.01;
