db8d259666a6d2bdaabaef8b12fd623734e43da7
Andrew Barna <abarna@gmail.com>
2020-07-11 22:06:00 +0000

    Set the y range of the spectrum plot manually

--- src/ui/plot.jsx
+++ src/ui/plot.jsx
@@ -20,7 +20,7 @@ export function NuSpectrumPlot({ cores, spectrum, detector, crossSection }) {
   const closestActiveIAEACoreSignal =
     closestActiveIAEACore?.detectorSignal || new Float32Array(1000).fill(0);
 
-  const customCores = coreList.filter((core) => core.custom === true)
+  const customCores = coreList.filter((core) => core.custom === true);
   const customCoreSignal = customCores.reduce((previous, current) => {
     return previous.map(
       (value, index) => value + current.detectorSignal[index]
@@ -46,7 +46,7 @@ export function NuSpectrumPlot({ cores, spectrum, detector, crossSection }) {
       mode: "lines",
       fill: "tozerox",
       marker: { color: "yellow" },
-      line: {width: 0.7},
+      line: { width: 0.7 },
       visible: sum(spectrum.geoK) > 0,
     },
     {
@@ -57,7 +57,7 @@ export function NuSpectrumPlot({ cores, spectrum, detector, crossSection }) {
       mode: "lines",
       fill: "tozerox",
       marker: { color: "blue" },
-      line: {width: 0.7},
+      line: { width: 0.7 },
       visible: sum(spectrum.geoU) > 0,
     },
     {
@@ -68,7 +68,7 @@ export function NuSpectrumPlot({ cores, spectrum, detector, crossSection }) {
       mode: "lines",
       fill: "tozerox",
       marker: { color: "red" },
-      line: {width: 0.7},
+      line: { width: 0.7 },
       visible: sum(spectrum.geoTh) > 0,
     },
     {
@@ -79,7 +79,7 @@ export function NuSpectrumPlot({ cores, spectrum, detector, crossSection }) {
       mode: "lines",
       fill: "tozerox",
       marker: { color: "green" },
-      line: {width: 0.7},
+      line: { width: 0.7 },
       visible: sum(totalCoreSignal) > 0,
     },
     {
@@ -93,12 +93,16 @@ export function NuSpectrumPlot({ cores, spectrum, detector, crossSection }) {
     },
   ];
 
+  const ymax = Math.max(...data.map((series) => Math.max(...series.y)));
+
   const layout = {
     title: `Antineutrino Spectrum: ${
       ["custom", "follow"].includes(detector.current)
         ? "Custom Location"
         : detector.current
-    } (${detector.lat.toFixed(1)}N, ${detector.lon.toFixed(1)}E, ${detector.elevation.toFixed(0)}m)`,
+    } (${detector.lat.toFixed(1)}N, ${detector.lon.toFixed(
+      1
+    )}E, ${detector.elevation.toFixed(0)}m)`,
     showlegend: true,
     legend: {
       x: 1,
@@ -110,20 +114,19 @@ export function NuSpectrumPlot({ cores, spectrum, detector, crossSection }) {
       title: { text: `Antineutrino Energy E (MeV)` },
     },
     yaxis: {
-      rangemode: "nonnegative",
-      autorange: true,
+      range: [0, ymax * 1.05],
       title: { text: `Rate dR/dE (NIU/MeV)<br /><sub>${crossSection}</sub>` },
     },
-    annotations: [            
-        {
-          showarrow: false,
-          text: 'geoneutrinos.org',
-          x: 1,                    
-          xref: 'paper',           
-          y: 0,                    
-          yref: 'paper'            
-        }
-      ]
+    annotations: [
+      {
+        showarrow: false,
+        text: "geoneutrinos.org",
+        x: 1,
+        xref: "paper",
+        y: 0,
+        yref: "paper",
+      },
+    ],
   };
   return (
     <Plot
@@ -137,19 +140,27 @@ export function NuSpectrumPlot({ cores, spectrum, detector, crossSection }) {
 }
 
 export function CoreDirectionPlot({ cores, detector }) {
-  const coreArr = Object.values(cores)
+  const coreArr = Object.values(cores);
 
-  const PHWRcores = coreArr.filter((core) => core.spectrumType === "PHWR")
-  const GCRcores = coreArr.filter((core) => core.spectrumType === "GCR")
-  const LEUMoxCores = coreArr.filter((core) => core.spectrumType === "LEU_MOX")
-  const CustomCores = coreArr.filter((core) => core.spectrumType === "custom")
-  const AllOtherCores = coreArr.filter((core) => core.spectrumType !== "LEU_MOX" && core.spectrumType !== "GCR" && core.spectrumType !== "PHWR" && core.type !== "custom")
+  const PHWRcores = coreArr.filter((core) => core.spectrumType === "PHWR");
+  const GCRcores = coreArr.filter((core) => core.spectrumType === "GCR");
+  const LEUMoxCores = coreArr.filter((core) => core.spectrumType === "LEU_MOX");
+  const CustomCores = coreArr.filter((core) => core.spectrumType === "custom");
+  const AllOtherCores = coreArr.filter(
+    (core) =>
+      core.spectrumType !== "LEU_MOX" &&
+      core.spectrumType !== "GCR" &&
+      core.spectrumType !== "PHWR" &&
+      core.type !== "custom"
+  );
   const layout = {
     title: `Core Directions: ${
       ["custom", "follow"].includes(detector.current)
         ? "Custom Location"
         : detector.current
-    } (${detector.lat.toFixed(1)}N, ${detector.lon.toFixed(1)}E, ${detector.elevation.toFixed(0)}m)`,
+    } (${detector.lat.toFixed(1)}N, ${detector.lon.toFixed(
+      1
+    )}E, ${detector.elevation.toFixed(0)}m)`,
     showlegend: true,
     autosize: true,
     legend: {
@@ -158,93 +169,94 @@ export function CoreDirectionPlot({ cores, detector }) {
       y: 1,
       yanchor: "top",
     },
-    margin :{
-      b: 110
+    margin: {
+      b: 110,
     },
-    annotations: [            
-        {
-          showarrow: false,
-          align:"right",
-          text: 'geoneutrinos.org<br />Angular Axis: Azimuth (deg): East is 0 deg, North is 90 deg, etc.<br />Radial Axis: Altitude (deg): Horizon 0 deg, Nadir -90 deg',
-          x: 1.2,
-          xref: 'paper',
-          y: -0.075,
-          yref: 'paper',
-          yanchor: "top",
-          xanchor: "right"
-        }
-      ]
+    annotations: [
+      {
+        showarrow: false,
+        align: "right",
+        text:
+          "geoneutrinos.org<br />Angular Axis: Azimuth (deg): East is 0 deg, North is 90 deg, etc.<br />Radial Axis: Altitude (deg): Horizon 0 deg, Nadir -90 deg",
+        x: 1.2,
+        xref: "paper",
+        y: -0.075,
+        yref: "paper",
+        yanchor: "top",
+        xanchor: "right",
+      },
+    ],
   };
   return (
     <div>
-    <Plot
-      data={[
-        {
-          name: "All Other Cores", //All other
-          type: "scatterpolar",
-          r: AllOtherCores.map((core) => core.direction.elev),
-          theta: AllOtherCores.map((core) => core.direction.phi),
-          text: AllOtherCores.map((core) => `${core.name} (${core.type})`),
-          mode: "markers",
-          hoverinfo: "text",
-          marker: {
-            color: "#009000"
-          }
-        },
-        {
-          name: "Custom Cores",
-          type: "scatterpolar",
-          r: CustomCores.map((core) => core.direction.elev),
-          theta: CustomCores.map((core) => core.direction.phi),
-          text: CustomCores.map((core) => core.name),
-          mode: "markers",
-          hoverinfo: "text",
-          marker: {
-            color: "#000"
-          }
-        },
-        {
-          name: "GCR Cores", //GCR Cores
-          type: "scatterpolar",
-          r: GCRcores.map((core) => core.direction.elev),
-          theta: GCRcores.map((core) => core.direction.phi),
-          text: GCRcores.map((core) => `${core.name} (${core.type})`),
-          mode: "markers",
-          hoverinfo: "text",
-          marker: {
-            color: "#D69537"
-          }
-        },
-        {
-          name: "LEU MOX Cores", //LEU MOX COres
-          type: "scatterpolar",
-          r: LEUMoxCores.map((core) => core.direction.elev),
-          theta: LEUMoxCores.map((core) => core.direction.phi),
-          text: LEUMoxCores.map((core) => `${core.name} (${core.type} MOX)`),
-          mode: "markers",
-          hoverinfo: "text",
-          marker: {
-            color: "#0000ff"
-          }
-        },
-        {
-          name: "PHWR Cores", //PHWR Cores
-          type: "scatterpolar",
-          r: PHWRcores.map((core) => core.direction.elev),
-          theta: PHWRcores.map((core) => core.direction.phi),
-          text: PHWRcores.map((core) => `${core.name} (${core.type})`),
-          mode: "markers",
-          hoverinfo: "text",
-          marker: {
-            color: "#ff0000"
-          }
-        },
-      ]}
-      useResizeHandler={true}
-      style={{ width: "100%", height: "500px", minHeight: "400px"}}
-      config={{ toImageButtonOptions: { width: 900, height: 900, scale: 2 } }}
-      layout={layout}
-    />
+      <Plot
+        data={[
+          {
+            name: "All Other Cores", //All other
+            type: "scatterpolar",
+            r: AllOtherCores.map((core) => core.direction.elev),
+            theta: AllOtherCores.map((core) => core.direction.phi),
+            text: AllOtherCores.map((core) => `${core.name} (${core.type})`),
+            mode: "markers",
+            hoverinfo: "text",
+            marker: {
+              color: "#009000",
+            },
+          },
+          {
+            name: "Custom Cores",
+            type: "scatterpolar",
+            r: CustomCores.map((core) => core.direction.elev),
+            theta: CustomCores.map((core) => core.direction.phi),
+            text: CustomCores.map((core) => core.name),
+            mode: "markers",
+            hoverinfo: "text",
+            marker: {
+              color: "#000",
+            },
+          },
+          {
+            name: "GCR Cores", //GCR Cores
+            type: "scatterpolar",
+            r: GCRcores.map((core) => core.direction.elev),
+            theta: GCRcores.map((core) => core.direction.phi),
+            text: GCRcores.map((core) => `${core.name} (${core.type})`),
+            mode: "markers",
+            hoverinfo: "text",
+            marker: {
+              color: "#D69537",
+            },
+          },
+          {
+            name: "LEU MOX Cores", //LEU MOX COres
+            type: "scatterpolar",
+            r: LEUMoxCores.map((core) => core.direction.elev),
+            theta: LEUMoxCores.map((core) => core.direction.phi),
+            text: LEUMoxCores.map((core) => `${core.name} (${core.type} MOX)`),
+            mode: "markers",
+            hoverinfo: "text",
+            marker: {
+              color: "#0000ff",
+            },
+          },
+          {
+            name: "PHWR Cores", //PHWR Cores
+            type: "scatterpolar",
+            r: PHWRcores.map((core) => core.direction.elev),
+            theta: PHWRcores.map((core) => core.direction.phi),
+            text: PHWRcores.map((core) => `${core.name} (${core.type})`),
+            mode: "markers",
+            hoverinfo: "text",
+            marker: {
+              color: "#ff0000",
+            },
+          },
+        ]}
+        useResizeHandler={true}
+        style={{ width: "100%", height: "500px", minHeight: "400px" }}
+        config={{ toImageButtonOptions: { width: 900, height: 900, scale: 2 } }}
+        layout={layout}
+      />
     </div>
   );
 }
