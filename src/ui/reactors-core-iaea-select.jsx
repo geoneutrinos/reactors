b43506c761ac76ecd7725aa1a3539ccf58a7a8c4
Andrew Barna <abarna@gmail.com>
2020-09-29 08:54:36 -0700

    Update reactors-core-iaea-select.jsx

--- src/ui/reactors-core-iaea-select.jsx
+++ src/ui/reactors-core-iaea-select.jsx
@@ -61,10 +61,11 @@ export const CoreIAEARange = ({
       <Card.Header>IAEA Load Factor Date Range</Card.Header>
       <Card.Body>
         <p>
-          Monthly thermal power load factor (LF) data for the years 2003 through 2018 are from <a href="https://www.fe.infn.it/antineutrino/">INFN Antineutrinos</a>.
-          If the "Use IAEA LF Data" option (this is
-          the default) is selected for all or for individual cores, the thermal power is the average of the monthly LFs for
-          the selected Year-Month range.
+          Monthly thermal power load factor (LF) data for the years 2003 through 2018 are
+          from <a href="https://www.fe.infn.it/antineutrino/">INFN Antineutrinos</a>. If
+          the "Use IAEA LF Data" option (this is the default) is selected for all or for
+          individual cores, the thermal power is the average of the monthly LFs for the
+          selected Year-Month range.
         </p>
         <Form inline>
           <Form.Control
