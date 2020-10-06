66f22f33d46b2b775572d429424316c53f77e065
Andrew Barna <abarna@gmail.com>
2020-10-06 20:18:27 +0000

    Include dmsq32 in physics table

--- src/physics/neutrino-oscillation.ts
+++ src/physics/neutrino-oscillation.ts
@@ -15,8 +15,8 @@ export const dmsq31Inverted =  -2.435e-3;
 const c4t13Normal = (1 - s2t13Normal) * (1 - s2t13Normal);
 const c4t13Inverted = (1 - s2t13Inverted) * (1 - s2t13Inverted);
 
-const dmsq32Normal = dmsq31Normal - dmsq21;
-const dmsq32Inverted = dmsq31Inverted + dmsq21;
+export const dmsq32Normal = dmsq31Normal - dmsq21;
+export const dmsq32Inverted = dmsq31Inverted - dmsq21;
 
 const s22t12 = 4 * s2t12 * (1 - s2t12);
 const c2t12 = 1 - s2t12;
--- src/ui/physics-osc-params.tsx
+++ src/ui/physics-osc-params.tsx
@@ -8,6 +8,8 @@ import {
   s2t13Inverted,
   dmsq31Inverted,
   dmsq31Normal,
+  dmsq32Normal,
+  dmsq32Inverted,
   averageSurvivalProbabilityNormal,
   averageSurvivalProbabilityInverted,
 } from "../physics/neutrino-oscillation";
@@ -33,6 +35,10 @@ export const PhysicsOscillationPane = () => {
                 <sub>31</sub> (eV<sup>2</sup>)
               </th>
               <th>
+                δ<i>m</i><sup>2</sup>
+                <sub>32</sub> (eV<sup>2</sup>)
+              </th>
+              <th>
                 sin<sup>2</sup><i>θ</i><sub>12</sub>
               </th>
               <th>
@@ -48,6 +54,7 @@ export const PhysicsOscillationPane = () => {
               <td>Normal</td>
               <td>{dmsq21}</td>
               <td>{dmsq31Normal}</td>
+              <td>{dmsq32Normal}</td>
               <td>{s2t12.toFixed(3)}</td>
               <td>{s2t13Normal}</td>
               <td>{averageSurvivalProbabilityNormal.toFixed(4)}</td>
@@ -56,6 +63,7 @@ export const PhysicsOscillationPane = () => {
               <td>Inverted</td>
               <td>{dmsq21}</td>
               <td>{dmsq31Inverted}</td>
+              <td>{dmsq32Inverted}</td>
               <td>{s2t12.toFixed(3)}</td>
               <td>{s2t13Inverted}</td>
               <td>{averageSurvivalProbabilityInverted.toFixed(4)}</td>
