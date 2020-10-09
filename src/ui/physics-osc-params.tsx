904a748bf45ff3a9ad0e54632406bcf375cba488
Stephen Dye <47828035+stdye@users.noreply.github.com>
2020-10-08 17:27:49 -1000

    Update physics-osc-params.tsx

--- src/ui/physics-osc-params.tsx
+++ src/ui/physics-osc-params.tsx
@@ -17,44 +17,30 @@ export const PhysicsOscillationPane = () => {
     <Card>
       <Card.Header>Neutrino Oscillation Parameters</Card.Header>
       <Card.Body>
-          <p>Osciliation values are from table 14.1 in <a href="https://pdg.lbl.gov/2017/reviews/rpp2017-rev-neutrino-mixing.pdf">C. Patrignani et al. (Particle Data Group), Chin. Phys. C, 40, 100001 (2016) and 2017 update</a></p>
+          <p>Parameter values are from table 14.7 in <a href="https://pdg.lbl.gov/2020/reviews/rpp2020-rev-neutrino-mixing.pdf">Particle Data Group</a></p>
         <Table {...tableProps}>
           <thead>
             <tr>
               <th></th>
-              <th>Normal</th>
-              <th>Inverted</th>
+              <th> Δ<i>m</i><sup>2</sup><sub>21</sub>
+              <th> Δ<i>m</i><sup>2</sup><sub>31</sub>
+              <th> sin<sup>2</sup><i>θ</i><sub>12</sub>
+              <th> sin<sup>2</sup><i>θ</i><sub>13</sub>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td>
-                Δ<i>m</i><sup>2</sup>
-                <sub>21</sub>
-              </td>
+              <td>Normal</td>
               <td>{dmsq21}</td>
-              <td>{dmsq21}</td>
-            </tr>
-            <tr>
-              <td>
-                Δ<i>m</i><sup>2</sup>
-                <sub>31</sub>
-              </td>
               <td>{dmsq31Normal}</td>
-              <td>{dmsq31Inverted}</td>
-            </tr>
-            <tr>
-              <td>
-                sin<sup>2</sup><i>θ</i><sub>12</sub>
-              </td>
               <td>{s2t12}</td>
-              <td>{s2t12}</td>
-            </tr>
-            <tr>
-              <td>
-                sin<sup>2</sup><i>θ</i><sub>13</sub>
-              </td>
               <td>{s2t13Normal}</td>
+            </tr>
+            <tr>
+              <td>Inverted</td>
+              <td>{dmsq21}</td>
+              <td>{dmsq31Inverted}</td>
+              <td>{s2t12}</td>
               <td>{s2t13Inverted}</td>
             </tr>
           </tbody>
