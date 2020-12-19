92a1ec785a3d1482915f88b37d2f5a06fed2bc8e
Andrew Barna <abarna@gmail.com>
2020-12-19 00:04:48 +0000

    remove unused import

--- src/ui/detector-physics.jsx
+++ src/ui/detector-physics.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { Card, Form } from "react-bootstrap";
 
 import { MassOrdering } from "../physics/neutrino-oscillation";
