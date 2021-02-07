ddd1827f167f0b315f3b220219ca2431e9ac19d6
Andrew Barna <abarna@gmail.com>
2021-02-07 18:30:55 +0000

    Get the corodinate transform correct in the analemma

--- src/ui/solar-plots.jsx
+++ src/ui/solar-plots.jsx
@@ -3,80 +3,69 @@ import React from "react";
 import { Card } from "react-bootstrap";
 import Plot from "react-plotly.js";
 
-import {range} from 'lodash';
+import { range } from "lodash";
 
-import {SECONDS_PER_YEAR} from "../physics/constants"
+import { SECONDS_PER_YEAR } from "../physics/constants";
 import { boron8Bins } from "../solar";
-import {detectorSunPosition} from '../detectors'
-
+import { detectorSunPosition } from "../detectors";
 
 const times = range(0, 24).map((hour) => {
-  return range(0,365, 5).map((jd) => {
-    let d = new Date("2021-01-01T00:00:00Z")
-    d.setUTCDate(jd)
-    d.setUTCHours(hour)
-    return d
-  })
-})
+  return range(0, 365, 10).map((jd) => {
+    let d = new Date("2021-01-01T00:30:00Z");
+    d.setUTCDate(jd);
+    d.setUTCHours(hour);
+    return d;
+  });
+});
 
-export const AnalemmaPlot = ({detector, cores}) => {
-  //const ana = times[1].map((date) => detectorSunPosition(detector, date))
-  //const data = [
-  //  {
-  //    y: ana.map(v => v.altitude),
-  //    x: ana.map(v => v.azimuth),
-  //    name: "solar",
-  //    type: "scatter",
-  //    mode: "lines",
-  //    fill: "none",
-  //    marker: { color: "blue" },
-  //  },
-  //];
-  let data = times.map(days => {
-    let ana = days.map(date => detectorSunPosition(detector, date))
+export const AnalemmaPlot = ({ detector, cores }) => {
+  let data = times.map((days) => {
+    let fakeDetector = { ...detector, lon: 0 };
+    let ana = days.map((date) => detectorSunPosition(fakeDetector, date));
+    let x = ana.map((v) => v.azimuth);
+    let y = ana.map((v) => v.altitude);
+    x.push(x[0]);
+    y.push(y[0]);
     return {
-      y: ana.map(v => v.altitude),
-      x: ana.map(v => v.azimuth),
+      y: y,
+      x: x,
       name: "solar",
       type: "scatter",
-      mode: "markers",
+      mode: "lines",
       fill: "none",
       marker: { color: "blue" },
-    }
-  })
+    };
+  });
   console.log({
-    y: Object.values(cores).map(v => v.elevation),
-    x: Object.values(cores).map(v => v.phi),
+    y: Object.values(cores).map((v) => v.elevation),
+    x: Object.values(cores).map((v) => v.phi),
     name: "cores",
     type: "scatter",
     mode: "markers",
     fill: "none",
     marker: { color: "green" },
-})
+  });
   data.push({
-      y: Object.values(cores).map(v => v.direction.elev * (Math.PI/180)),
-      x: Object.values(cores).map(v => -v.direction.phi * (Math.PI/180)),
-      name: "cores",
-      type: "scatter",
-      mode: "markers",
-      fill: "none",
-      marker: { color: "green" },
-  })
+    y: Object.values(cores).map((v) => v.direction.elev * (Math.PI / 180)),
+    x: Object.values(cores)
+      .map((v) => -(v.direction.phi * (Math.PI / 180)) - Math.PI / 2)
+      .map((v) => (v < -Math.PI ? v + 2 * Math.PI : v)),
+    name: "cores",
+    type: "scatter",
+    mode: "markers",
+    fill: "none",
+    marker: { color: "green" },
+  });
   var layout = {
     title: "Solar Analemma",
     autosize: true,
-    //xaxis: {
-    //  range: [ -Math.PI, Math.PI ]
-    //},
-    //yaxis: {
-    //  range: [ -Math.PI/2, Math.PI/2 ]
-    //},
-    showlegend: false
-   // legend: {
-   //   x: 1,
-   //   xanchor: "right",
-   //   y: 0,
-   // },
+    xaxis: {
+      range: [-Math.PI, Math.PI],
+    },
+    yaxis: {
+      range: [-Math.PI / 2, Math.PI / 2],
+    },
+    showlegend: false,
   };
   return (
     <Card>
@@ -93,7 +82,7 @@ export const AnalemmaPlot = ({detector, cores}) => {
   );
 };
 
-export const Boron8SpectraPlot = ({boron8}) => {
+export const Boron8SpectraPlot = ({ boron8 }) => {
   const data = [
     {
       y: boron8.boron8Rate.map((x) => x * 1e1 * SECONDS_PER_YEAR * 1e32),
@@ -123,7 +112,9 @@ export const Boron8SpectraPlot = ({boron8}) => {
   };
   return (
     <Card>
-      <Card.Header><sup>8</sup>B Solar Neutrinos</Card.Header>
+      <Card.Header>
+        <sup>8</sup>B Solar Neutrinos
+      </Card.Header>
       <Card.Body>
         <p>
           R<sub>sol</sub> = {boron8.boron8NIU.toFixed(2)} NIU
@@ -131,12 +122,13 @@ export const Boron8SpectraPlot = ({boron8}) => {
         <p>
           <sup>8</sup>B decay spectrum from:
           <br />
-          W. T. Winter et al., "The <sup>8</sup>B neutrino spectrum," Phys. Rev. C 73,
-          025503 (2006).
+          W. T. Winter et al., "The <sup>8</sup>B neutrino spectrum," Phys. Rev.
+          C 73, 025503 (2006).
         </p>
 
         <p>
-          <sup>8</sup>B decay solar neutrino flux (2.345x10<sup>6</sup> cm<sup>-2</sup>s<sup>-1</sup>) from:
+          <sup>8</sup>B decay solar neutrino flux (2.345x10<sup>6</sup> cm
+          <sup>-2</sup>s<sup>-1</sup>) from:
           <br />
           K. Abe et al., "Solar neutrino measurements in Super-Kamiokande-IV,"
           Phys. Rev. D 94, 052010 (2016).
