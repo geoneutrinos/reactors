f89ae90571b618f062ecaaaf6b6b73ff8d476d5f
Stephen Dye <47828035+stdye@users.noreply.github.com>
2021-02-18 15:51:36 -1000

    Update solar-plots.jsx

--- src/ui/solar-plots.jsx
+++ src/ui/solar-plots.jsx
@@ -97,7 +97,6 @@ export const AnalemmaPlot = ({ detector, cores, reactorLF}) => {
       name: "solar",
       type: "scattergl",
       mode: "markers",
-      hoverinfo: "x+y",
       opacity: 0.5,
       fill: "none",
       marker: { size: 3, color: z, colorscale:'RdBu', cmin:0.98329, cmax: 1.01671, colorbar: {thickness: 15, title: "Earth-Sun (au)" }},
@@ -111,6 +110,7 @@ export const AnalemmaPlot = ({ detector, cores, reactorLF}) => {
 
   var layout = {
     title: "Solar Analemma",
+    hovermode: "closest",
     autosize: true,
     xaxis: {
       title: "γ (deg)",
