5947cc83b53ba0250974221d32ac774c57ff083b
Stephen Dye <47828035+stdye@users.noreply.github.com>
2021-03-09 14:40:50 -1000

    Update plot.jsx

--- src/ui/plot.jsx
+++ src/ui/plot.jsx
@@ -189,8 +189,8 @@ export function CoreDirectionPlot({ cores, detector }) {
         showarrow: false,
         align: "right",
         text:
-          "geoneutrinos.org &nbsp; &nbsp; <br />Angular Axis is Azimuth (deg): North is 0&deg;, East is 90&deg;, etc. &nbsp; &nbsp; <br />Radial Axis is Elevation (deg): Horizon 0&deg;, Nadir -90&deg; &nbsp; &nbsp; ",
-        x: 1.2,
+          "geoneutrinos.org <br />Angular Axis is Azimuth (deg): North is 0&deg;, East is 90&deg;, etc. <br />Radial Axis is Elevation (deg): Horizon 0&deg;, Nadir -90&deg; ",
+        x: 1.1,
         xref: "paper",
         y: -0.075,
         yref: "paper",
