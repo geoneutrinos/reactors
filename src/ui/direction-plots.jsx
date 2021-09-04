6652dcbfd17c27868b3dccf0369ff272d145fe13
Stephen Dye <47828035+stdye@users.noreply.github.com>
2021-09-04 11:10:56 -1000

    add CoreDirectionSignalPlots

--- src/ui/direction-plots.jsx
+++ src/ui/direction-plots.jsx
@@ -135,3 +135,151 @@ export function CoreDirectionPlot({ cores, detector }) {
     </div>
   );
 }
+
+export const CoreDirectionSignalPlots = ({ cores, detector, reactorLF }) => {
+  const sortedCores = Object.values(cores)
+    .filter((a) => a.detectorNIU > 0)
+    .sort((a, b) => b.detectorNIU - a.detectorNIU);
+  let [first, ...rest] = sortedCores;
+  let coreData = []
+  if (first !== undefined){
+   coreData = [first, ...rest].map((core) => {
+    return { y: core, x: first.cos(core) };
+   });
+  } else {
+    first = {"name": "No Cores"}
+  }
+
+  const PHWRcores = coreData.filter((core) => core.y.spectrumType === "PHWR");
+  const GCRcores = coreData.filter((core) => core.y.spectrumType === "GCR");
+  const LEUMoxCores = coreData.filter(
+    (core) => core.y.spectrumType === "LEU_MOX"
+  );
+  const CustomCores = coreData.filter(
+    (core) => core.y.spectrumType === "custom"
+  );
+  const AllOtherCores = coreData.filter(
+    (core) =>
+      core.y.spectrumType !== "LEU_MOX" &&
+      core.y.spectrumType !== "GCR" &&
+      core.y.spectrumType !== "PHWR" &&
+      core.y.type !== "custom"
+  );
+  const data = [
+    {
+      y: AllOtherCores.map((d) => d.y.detectorNIU),
+      x: AllOtherCores.map((d) => d.x),
+      text: AllOtherCores.map((core) => `${core.y.name} (${core.y.type})<br>cosθ= ${core.x.toFixed(3)}<br>signal= ${core.y.detectorNIU.toExponential(2)}`),
+      name: `PWR,BWR`,
+      type: "scatter",
+      mode: "markers",
+      hoverinfo: "text",
+      marker: {
+        color: "#009000",
+      },
+    },
+    {
+      name: "Custom",
+      type: "scatter",
+      y: CustomCores.map((d) => d.y.detectorNIU),
+      x: CustomCores.map((d) => d.x),
+      text: CustomCores.map((core) => `${core.y.name} (${core.y.type})<br>cosθ= ${core.x.toFixed(3)}<br>signal= ${core.y.detectorNIU.toExponential(2)}`),
+      mode: "markers",
+      hoverinfo: "text",
+      marker: {
+        color: "#000",
+      },
+    },
+    {
+      name: "GCR", //GCR Cores
+      type: "scatter",
+      y: GCRcores.map((d) => d.y.detectorNIU),
+      x: GCRcores.map((d) => d.x),
+      text: GCRcores.map((core) => `${core.y.name} (${core.y.type})<br>cosθ= ${core.x.toFixed(3)}<br>signal= ${core.y.detectorNIU.toExponential(2)}`),
+      mode: "markers",
+      hoverinfo: "text",
+      marker: {
+        color: "#D69537",
+      },
+    },
+    {
+      name: "PWR/MOX", //LEU MOX COres
+      type: "scatter",
+      y: LEUMoxCores.map((d) => d.y.detectorNIU),
+      x: LEUMoxCores.map((d) => d.x),
+      text: LEUMoxCores.map((core) => `${core.y.name} (${core.y.type})<br>cosθ= ${core.x.toFixed(3)}<br>signal= ${core.y.detectorNIU.toExponential(2)}`),
+      mode: "markers",
+      hoverinfo: "text",
+      marker: {
+        color: "#0000ff",
+      },
+    },
+    {
+      name: "PHWR", //PHWR Cores
+      type: "scatter",
+      y: PHWRcores.map((d) => d.y.detectorNIU),
+      x: PHWRcores.map((d) => d.x),
+      text: PHWRcores.map((core) => `${core.y.name} (${core.y.type})<br>cosθ= ${core.x.toFixed(3)}<br>signal= ${core.y.detectorNIU.toExponential(2)}`),
+      mode: "markers",
+      hoverinfo: "text",
+      marker: {
+        color: "#ff0000",
+      },
+    },
+  ];
+  var layout = {
+    hovermode: "closest",
+    title: `Core Directions w.r.t. ${
+      ["custom", "follow"].includes(detector.current)
+        ? "Custom Location"
+        : detector.current
+    } to ${first.name}<br /><sub>(${reactorLF.start.toISOString().slice(0, 7)} through ${reactorLF.end.toISOString().slice(0, 7)} avg Load Factor)</sub>`,
+    yaxis: {
+      title: { text: `signal (NIU)` },
+      type: 'log',
+      autorange: true
+    },
+    xaxis: {
+      range: [-1.05 , 1.05],
+      zeroline: false,
+      title: { text: `cosθ` },
+    },
+    autosize: true,
+    legend: {
+      x: 1,
+      y: 1,
+    },
+    annotations: [
+      {
+        showarrow: false,
+        text: "geoneutrinos.org",
+        x: 1.1,
+        xref: "paper",
+        y: -0.15,
+        yref: "paper",
+      },
+    ],
+  };
+  var config = {
+    toImageButtonOptions: {
+      filename: 'Core-Directions-signal-vs-cos(theta)'
+    }
+  };
+  return (
+    <Card>
+      <Card.Header>Core Directions Plot</Card.Header>
+      <Card.Body>
+        <p> Reactor core signals in NIU versus the cosine of the angle &theta; w.r.t. the direction from the detector to the core with the highest signal (1 NIU = 1 interaction/10<sup>32</sup>{" "}
+            targets/year).
+        </p>
+        <Plot
+          useResizeHandler={true}
+          style={{ width: "100%" }}
+          data={data}
+          layout={layout}
+          config={config}
+        />
+      </Card.Body>
+    </Card>
+  );
+};
