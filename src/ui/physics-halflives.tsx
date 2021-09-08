dd7badd3672da8e6b9d22136cbb29d5d1f866e79
Stephen Dye <47828035+stdye@users.noreply.github.com>
2021-09-08 11:59:08 -1000

    Create physics-halflives.tsx

---
+++ src/ui/physics-halflives.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import {ISOTOPIC_HALF_LIFE} from '../physics/constants'
+
+import { Card, Table } from 'react-bootstrap';
+
+export const IsotopeHalfLives = () => {
+  return (
+    <Card>
+      <Card.Header>Isotope Half Lives</Card.Header>
+      <Card.Body>
+        <p>The listed half life values in 10<sup>9</sup> y are from the Nuclear Data Sheets.</p>
+        <Table>
+            <thead>
+              <tr>
+                <th><sup>40</sup>K)</th>
+                <th><sup>232</sup>Th)</th>
+                <th><sup>232</sup>U)</th>
+                <th><sup>238</sup>U)</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  {K40:}
+                </td>
+                <td>
+                  {Th232:}
+                </td>
+                <td>
+                  {U235:}
+                </td>
+                <td>
+                  {U238:}
+                </td>
+              </tr>
+            </tbody>
+          </Table>
+      </Card.Body>
+    </Card>
+  );
+}
