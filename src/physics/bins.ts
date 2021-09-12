9b17d56901adf16647220c24a42951d15dce4cd4
Andrew Barna <abarna@gmail.com>
2021-09-12 18:47:19 +0000

    Define bin center in terms of IBD Threshold

--- src/physics/bins.ts
+++ src/physics/bins.ts
@@ -1,9 +1,16 @@
+import {IBD_THRESHOLD} from "./derived"
+
 export const binStart = 0 // MeV
 export const binEnd = 10 // MeV
 export const binCount = 1000
-export const binAlign = 0.107 // number between 0 and 1, 0 is left edge, 1 is right edge
+//export const binAlign = 0.107 // number between 0 and 1, 0 is left edge, 1 is right edge
 
 export const binWidth = (binEnd - binStart) / binCount;
+
+// This figures out which bin the IBD threshold is in and ensure that the bin center is always 0.5 binwidths away
+const IBD_bin_remainder = IBD_THRESHOLD/binWidth - Math.floor(IBD_THRESHOLD/binWidth)
+// we always want the number to be between 0 and 1
+export const binAlign = IBD_bin_remainder < 0.5? IBD_bin_remainder + 0.5 : IBD_bin_remainder - 0.5
 
 const calcBins = (binStart:number, binEnd:number, binCount:number, binAlign:number):Float64Array => {
   const offset = binWidth * binAlign;
