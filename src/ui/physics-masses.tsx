0cb7e77eaec9bf8ea2b026b37263968b35487ebc
Stephen Dye <47828035+stdye@users.noreply.github.com>
2021-09-12 07:22:54 -1000

    List IBD_THRESHOLD on masses table

--- src/ui/physics-masses.tsx
+++ src/ui/physics-masses.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import {NEUTRON_REST_MASS, PROTON_REST_MASS, ELECTRON_REST_MASS} from '../physics/constants'
 
+import IBD_THRESHOLD from '../physics/derived'
+
 import { Card, Table } from 'react-bootstrap';
 
 export const ParticleMasses = () => {
   return (
     <Card>
-      <Card.Header>Particle Masses</Card.Header>
+      <Card.Header>Particle Masses / IBD Threshold</Card.Header>
       <Card.Body>
         <p>The listed values are from the Particle Data Group <a href="https://pdg.lbl.gov/2020/reviews/rpp2020-rev-phys-constants.pdf">at this link</a>.</p>
         <Table>
@@ -16,6 +18,7 @@ export const ParticleMasses = () => {
                 <th><i>m</i><sub>n</sub> (MeV)</th>
                 <th><i>m</i><sub>p</sub> (MeV)</th>
                 <th><i>m</i><sub>e</sub> (MeV)</th>
+                <th><i>E</i><sub>thesh</sub> (MeV)</th>
               </tr>
             </thead>
             <tbody>
@@ -28,6 +31,9 @@ export const ParticleMasses = () => {
                 </td>
                 <td>
                   {ELECTRON_REST_MASS}
+                </td>
+                <td>
+                  {IBD_THRESHOLD}
                 </td>
               </tr>
             </tbody>
