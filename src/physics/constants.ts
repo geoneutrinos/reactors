a5fc462881bf0214d1664e3dd418f5bf2abec19e
Stephen Dye <47828035+stdye@users.noreply.github.com>
2021-09-15 07:31:10 -1000

    bigger change T_1/2 Th

--- src/physics/constants.ts
+++ src/physics/constants.ts
@@ -52,7 +52,7 @@ export const ISOTOPIC_NATURAL_ABUNDANCE = {
 export const ISOTOPIC_HALF_LIFE = {
   // Gy -> Seconds
   K40: 1.248 * 1e9 * SECONDS_PER_YEAR, // J. Chen, Nuclear Data Sheets A=40 140 (2017) 
-  TH232: 15.0 * 1e9 * SECONDS_PER_YEAR, // E. Browne, Nuclear Data Shetts A=232 107 (2006)
+  TH232: 19.0 * 1e9 * SECONDS_PER_YEAR, // E. Browne, Nuclear Data Shetts A=232 107 (2006)
   U235: 0.704 * 1e9 * SECONDS_PER_YEAR, // E. Browne and J.K. Tuli, Nuclear Data Sheets A=235 122 (2014)
   U238: 4.468 * 1e9 * SECONDS_PER_YEAR, // E. Browne and J.K. Tuli, Nuclear Data Sheets A=238 127 (2015)
   // Gy
