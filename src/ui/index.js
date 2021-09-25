233802e76526221ba81dd722bd713ae805b9cb44
Stephen Dye <47828035+stdye@users.noreply.github.com>
2021-09-25 12:50:28 -1000

    Update index.js

--- src/ui/index.js
+++ src/ui/index.js
@@ -15,7 +15,7 @@ export { PhysicsOscillationPane } from './physics-osc-params'
 export { PhysicsConstants } from './physics-constants';
 export { IsotopeHalfLives } from './physics-halflives';
 export { ParticleMasses } from './physics-masses';
-export { CrossSectionPlots, DifferentialCrossSectionPlots, AngularDifferentialCrossSectionPlots, CDFdifferentialCrossSectionPlots, CDFAngularDifferentialCrossSectionPlots } from './physics-plots';
+export { CrossSectionPlots, DifferentialCrossSectionPlots, DifferentialCrossSectionPlotsNeutrinos, AngularDifferentialCrossSectionPlots, CDFdifferentialCrossSectionPlots, CDFAngularDifferentialCrossSectionPlots } from './physics-plots';
 export { FissionIsotopeSpectraPlots, CoreDirectionSignalPlots } from './reactors-plots';
 export { Boron8SpectraPlot, AnalemmaPlot } from './solar-plots';
 export { FissionFractionPane } from './reactors-fission';
