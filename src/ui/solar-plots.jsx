e141f9af1107e75e3ffe1eb3fa53974b2b0bf377
Andrew Barna <abarna@gmail.com>
2021-09-30 22:10:27 +0000

    Move Boron8 KE calcualtions to a more global location

--- src/ui/solar-plots.jsx
+++ src/ui/solar-plots.jsx
@@ -1,17 +1,13 @@
-import React, {useContext} from "react";
+import React from "react";
 
 import { Card } from "react-bootstrap";
 import Plot from "react-plotly.js";
 
-import { range, sum, zip } from "lodash";
+import { range } from "lodash";
 
 import { SECONDS_PER_YEAR } from "../physics/constants";
 import { boron8Bins } from "../solar";
 import { detectorSunPosition } from "../detectors";
-import { crossSectionElasticScattering, NeutrinoType } from "../physics/neutrino-cross-section";
-
-import { PhysicsContext } from "../state";
-
 
 const plotDef = (cores, color) => {
   return {
@@ -33,15 +29,20 @@ const earthSunDist = (date) => {
   const JD = date.valueOf() / 86400000 - 0.5 + 2440588;
   const n = JD - 2451545;
   const g = 357.528 + 0.9856003 * n;
-  const R = 1.00014 - 0.01671 * Math.cos(g * Math.PI/180) - 0.00014 * Math.cos(g * Math.PI/180) ** 2;
+  const R =
+    1.00014 -
+    0.01671 * Math.cos((g * Math.PI) / 180) -
+    0.00014 * Math.cos((g * Math.PI) / 180) ** 2;
   return R;
-}
+};
 
-export const AnalemmaPlot = ({ detector, cores, reactorLF}) => {
+export const AnalemmaPlot = ({ detector, cores, reactorLF }) => {
   // Some cals for filtering what times are shown
-  let yearOrMore = ((reactorLF.end - reactorLF.start) >= 28857600000);
-  let startMonth = (reactorLF.start.getUTCMonth() + 1).toString().padStart(2, "0")
-  let endMonth = (reactorLF.end.getUTCMonth() + 2).toString().padStart(2, "0")
+  let yearOrMore = reactorLF.end - reactorLF.start >= 28857600000;
+  let startMonth = (reactorLF.start.getUTCMonth() + 1)
+    .toString()
+    .padStart(2, "0");
+  let endMonth = (reactorLF.end.getUTCMonth() + 2).toString().padStart(2, "0");
   let start = new Date(`2021-${startMonth}-01T00:30:00Z`);
   let end = new Date(`2021-${endMonth}-01T00:30:00Z`);
   end.setUTCDate(0);
@@ -57,7 +58,7 @@ export const AnalemmaPlot = ({ detector, cores, reactorLF}) => {
         return d;
       }
       if (endMonth < startMonth) {
-        if ((d > end) && (d < start)){
+        if (d > end && d < start) {
           return undefined;
         }
       } else {
@@ -69,9 +70,9 @@ export const AnalemmaPlot = ({ detector, cores, reactorLF}) => {
         }
       }
       return d;
-    })
-    days.push(days[0])
-    return days
+    });
+    days.push(days[0]);
+    return days;
   });
   const coreData = Object.values(cores);
   const PHWRcores = coreData.filter((core) => core.spectrumType === "PHWR");
@@ -89,22 +90,39 @@ export const AnalemmaPlot = ({ detector, cores, reactorLF}) => {
   );
   let data = times.map((days) => {
     let fakeDetector = { ...detector, lon: 0 };
-    let ana = days.map((date) => date === undefined? date: detectorSunPosition(fakeDetector, date));
-    let x = ana.map((v) => v === undefined? v: ((v.azimuth + Math.PI) * 180) / Math.PI);
-    let y = ana.map((v) => v === undefined? v: (v.altitude * 180) / Math.PI);
-    let z = days.map((date) => date === undefined? date: 1/(earthSunDist(date))**2);
+    let ana = days.map((date) =>
+      date === undefined ? date : detectorSunPosition(fakeDetector, date)
+    );
+    let x = ana.map((v) =>
+      v === undefined ? v : ((v.azimuth + Math.PI) * 180) / Math.PI
+    );
+    let y = ana.map((v) =>
+      v === undefined ? v : (v.altitude * 180) / Math.PI
+    );
+    let z = days.map((date) =>
+      date === undefined ? date : 1 / earthSunDist(date) ** 2
+    );
     return {
       y: y,
       x: x,
       z: z,
-      text: z.map((zv) => `${zv === undefined? zv : zv.toFixed(5)} (1/au<sup>2</sup>)`),
+      text: z.map(
+        (zv) => `${zv === undefined ? zv : zv.toFixed(5)} (1/au<sup>2</sup>)`
+      ),
       hoverinfo: "text",
       name: "solar",
       type: "scattergl",
       mode: "markers",
       opacity: 0.5,
       fill: "none",
-      marker: { size: 3, color: z, colorscale:'Electric', cmin:0.96, cmax: 1.04, colorbar: {thickness: 15, title: "Intensity (1/au<sup>2</sup>)" }},
+      marker: {
+        size: 3,
+        color: z,
+        colorscale: "Electric",
+        cmin: 0.96,
+        cmax: 1.04,
+        colorbar: { thickness: 15, title: "Intensity (1/au<sup>2</sup>)" },
+      },
     };
   });
   data.push(plotDef(AllOtherCores, "#009000"));
@@ -121,7 +139,7 @@ export const AnalemmaPlot = ({ detector, cores, reactorLF}) => {
     } (${detector.lat.toFixed(1)}N, ${detector.lon.toFixed(
       1
     )}E, ${detector.elevation.toFixed(0)}m)`,
-//    title: "Solar Analemma",
+    //    title: "Solar Analemma",
     hovermode: "closest",
     autosize: true,
     xaxis: {
@@ -150,8 +168,8 @@ export const AnalemmaPlot = ({ detector, cores, reactorLF}) => {
   };
   var config = {
     toImageButtonOptions: {
-      filename: 'Solar-Analemma'
-    }
+      filename: "Solar-Analemma",
+    },
   };
   return (
     <Card>
@@ -170,31 +188,9 @@ export const AnalemmaPlot = ({ detector, cores, reactorLF}) => {
 };
 
 export const Boron8KEPlot = ({ boron8 }) => {
-  const {crossSection} = useContext(PhysicsContext)
-  const esTmin = crossSection.elasticScatteringTMin
-  const esSmear = (b8Rate) => {
-    const eVtoK = boron8Bins.map((bin) => {
-      const Tspec = boron8Bins.map((Tbin) =>
-        Tbin < esTmin? 0: 
-        crossSectionElasticScattering(
-          bin,
-          NeutrinoType.electronNeutrino,
-          Tbin - 0.05,
-          Tbin + 0.05
-        )
-      );
-      const totalT = sum(Tspec);
-      return Tspec.map((v) => totalT === 0? 0: v / totalT);
-    });
-    const newRates = b8Rate.map((v,i) => eVtoK[i].map(v2 => v * v2))
-    return zip(...newRates).map(v => sum(v))
-  };
-
-  const y = esSmear(boron8.boron8Rate).map((x) => x * 1e1 * SECONDS_PER_YEAR * 1e32)
-
   const data = [
     {
-      y: y,
+      y: boron8.boron8Ke,
       x: boron8Bins,
       name: "Boron 8",
       type: "scattergl",
@@ -231,8 +227,8 @@ export const Boron8KEPlot = ({ boron8 }) => {
   };
   var config = {
     toImageButtonOptions: {
-      filename: 'Solar-8B-ES-KE-Spectrum'
-    }
+      filename: "Solar-8B-ES-KE-Spectrum",
+    },
   };
   return (
     <Card>
@@ -292,8 +288,8 @@ export const Boron8SpectraPlot = ({ boron8 }) => {
   };
   var config = {
     toImageButtonOptions: {
-      filename: 'Solar-8B-ES-Rate-Spectrum'
-    }
+      filename: "Solar-8B-ES-Rate-Spectrum",
+    },
   };
   return (
     <Card>
@@ -318,10 +314,8 @@ export const Boron8SpectraPlot = ({ boron8 }) => {
           K. Abe et al., "Solar neutrino measurements in Super-Kamiokande-IV,"
           Phys. Rev. D 94, 052010 (2016).
         </p>
-  
-        <p>
-          Plotted data assume the flux is entirely electron neutrinos.
-        </p>
+
+        <p>Plotted data assume the flux is entirely electron neutrinos.</p>
         <Plot
           useResizeHandler={true}
           style={{ width: "100%" }}
