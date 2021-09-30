f77bd5769b6885d41f88fc0cbd363cf4ad306d63
Stephen Dye <47828035+stdye@users.noreply.github.com>
2021-09-30 13:13:38 -1000

    Update output-download.jsx

--- src/ui/output-download.jsx
+++ src/ui/output-download.jsx
@@ -158,14 +158,14 @@ export const OutputDownload = ({ cores, spectrum, detector, boron8 }) => {
         <DownloadButton
           data={{
             "bin center (MeV)": boron8Rate.map((_, i) => 0.1 + 0.1 * i),
-            "boron8 (NIU)": boron8Rate.map(
+            "8B Rate (NIU)": boron8Rate.map(
               (v) => v * 1e1 * SECONDS_PER_YEAR * 1e32
             ),
-            "boro8 ES Electron KE (MeV)": boron8Ke,
+            "8B Electron KE (MeV)": boron8Ke,
           }}
           formatters={{
-            "boron8 (NIU)": (v) => v.toPrecision(7),
-            "boro8 ES Electron KE (MeV)": (v) => v.toPrecision(7),
+            "8B Rate (NIU)": (v) => v.toPrecision(7),
+            "8B Electron KE (MeV)": (v) => v.toPrecision(7),
             ...downloadFormatters,
           }}
           filename={`SolarNu_spec100keV_ES_8Bsolar_Tmin${crossSection.elasticScatteringTMin.toFixed(
