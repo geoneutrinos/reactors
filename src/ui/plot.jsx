ae51131c5b99af9fb0d10a8dd82cf57259f5efd4
Stephen Dye <47828035+stdye@users.noreply.github.com>
2021-10-03 09:22:10 -1000

    try add NO/IO label

--- src/ui/plot.jsx
+++ src/ui/plot.jsx
@@ -140,7 +140,7 @@ export function NuSpectrumPlot({ cores, spectrum, detector, reactorLF, xaxisExtr
         : detector.current
     } (${detector.lat.toFixed(1)}N, ${detector.lon.toFixed(
       1
-    )}E, ${detector.elevation.toFixed(0)}m)<br /><sub>(${reactorLF.start.toISOString().slice(0, 7)} thru ${reactorLF.end.toISOString().slice(0, 7)} avg LF ; NuFitv5.0)</sub>`,
+    )}E, ${detector.elevation.toFixed(0)}m)<br /><sub>(${reactorLF.start.toISOString().slice(0, 7)} thru ${reactorLF.end.toISOString().slice(0, 7)} avg LF ; Osc- NuFit v5.0 ${oscillation.massOrdering})</sub>`,
     showlegend: true,
     legend: {
       x: 1,
