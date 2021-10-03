a813d66f870dfb615cd84a1a1a45f40fd3fe635d
Stephen Dye <47828035+stdye@users.noreply.github.com>
2021-10-02 18:12:35 -1000

    Update index.js

--- src/ui/index.js
+++ src/ui/index.js
@@ -4,6 +4,7 @@ export { NuMap } from "./map";
 export { NuSpectrumPlot, CoreDirectionPlot, KESpectrumPlot } from "./plot";
 export { StatsPanel } from "./detector-stats";
 export { DetectorPhysicsPane } from "./detector-physics";
+export { NeutrinoOscillationPane } from "./neutrino-oscillation";
 export { DetectorLocationPane } from "./detector-location";
 export { CoreList } from "./reactors-corelist";
 export { MantleFlux, CrustFlux, GeoNuSpectrumSource } from "./geonu";
