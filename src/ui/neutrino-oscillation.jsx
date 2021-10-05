3d35bb720bd2f19ab975e169cdd1c6ae44fe6bfa
Stephen Dye <47828035+stdye@users.noreply.github.com>
2021-10-05 11:52:40 -1000

    Update neutrino-oscillation.jsx

--- src/ui/neutrino-oscillation.jsx
+++ src/ui/neutrino-oscillation.jsx
@@ -43,9 +43,9 @@ export const NeutrinoOscillationPane = () => {
               <Node inline>{String.raw`\delta m_{ji}^2=m_j^2-m_i^2`}</Node> is the neutrino mass-squared difference in eV<sup>2</sup> and {" "}
               <Node inline>{String.raw`\theta_{12}`}</Node>, {" "}
               <Node inline>{String.raw`\theta_{13}`}</Node>, are the solar, reactor mixing angles, respectively. The complementary probability,{" "}
-              <Node inline>{String.raw`P_\mathrm{ee}(L,E_{{\nu}_\mathrm{e}}) = 1 - P_{\mathrm{e}x}`}</Node>, gauges survival of electron flavor. The Table
-              below lists the neutrino oscillation parameter values used to estimate the spectral distortion of reactor antineutrino reactions and the 
-              overall suppression of geo-neutrino reactions. An average survival probability, given by {" "}
+              <Node inline>{String.raw`P_\mathrm{ee}(L,E_{{\nu}_\mathrm{e}}) = 1 - P_{\mathrm{e}x}(L,E_{{\nu}_\mathrm{e}})`}</Node>, gauges survival 
+              of electron flavor. The Table below lists the neutrino oscillation parameter values used to estimate the spectral distortion of reactor 
+              antineutrino reactions and the overall suppression of geo-neutrino reactions. An average survival probability, given by {" "}
               <Node>{String.raw`<\!\!{P}_\mathrm{ee}\!\!> = 1- \frac{1} {2} \big(\cos^4 \theta_{13} \sin^2(2\theta_{12}) + \sin^2(2\theta_{13}) \big),`}</Node> 
               accounts for the effect of oscillations on geo-neutrinos.
             </p>
