48c5b6e99f6b41697477886a73adda5c7830fff1
Stephen Dye <47828035+stdye@users.noreply.github.com>
2021-10-05 11:11:02 -1000

    Update neutrino-oscillation.jsx

--- src/ui/neutrino-oscillation.jsx
+++ src/ui/neutrino-oscillation.jsx
@@ -35,7 +35,11 @@ export const NeutrinoOscillationPane = () => {
             <p> The probability that an electron neutrino or an electron antineutrino, assuming CPT invariance, of energy {" "} 
               <Node inline>{String.raw`E_{{\nu}_\mathrm{e}}`}</Node> in MeV changes flavor after traveling a distance {" "}
               <Node inline>{String.raw`L`}</Node> in meters is
-              <Node>{String.raw`P_{\mathrm{e}x}(L,E_{{\nu}_\mathrm{e}})=\cos^4\theta_{13}\sin^22\theta_{12}\sin^2(1.27\delta m^2_{21}L/E_{{\nu}_\mathrm{e}})`}</Node>{" "}
+              <Node>{String.raw`\begin{split}
+                P_{\mathrm{e}x}(L,E_{{\nu}_\mathrm{e}})=\cos^4\theta_{13}\sin^22\theta_{12}\sin^2(1.27\delta m^2_{21}L/E_{{\nu}_\mathrm{e}})\,\\
+                +\cos^2\theta_{12}\sin^22\theta_{13}\sin^2(1.27\delta m^2_{31}L/E_{{\nu}_\mathrm{e}})\,\\
+                +\sin^2\theta_{12}\sin^22\theta_{13}\sin^2(1.27\delta m^2_{32}L/E_{{\nu}_\mathrm{e}}),\\
+                \end{split}`}</Node>{" "}
             </p>
           </div>
         </Provider>
