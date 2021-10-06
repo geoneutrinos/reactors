1f7095a8e98a3ec18b1949f1ebfa367315681c11
Stephen Dye <47828035+stdye@users.noreply.github.com>
2021-10-05 14:04:39 -1000

    Update physics-masses.tsx

--- src/ui/physics-masses.tsx
+++ src/ui/physics-masses.tsx
@@ -4,6 +4,8 @@ import {NEUTRON_REST_MASS, PROTON_REST_MASS, ELECTRON_REST_MASS} from '../physic
 
 import {IBD_THRESHOLD} from '../physics/derived'
 
+import { Node, Provider } from "@nteract/mathjax";
+
 import { Card, Table } from 'react-bootstrap';
 
 export const ParticleMasses = () => {
@@ -11,6 +13,7 @@ export const ParticleMasses = () => {
     <Card>
       <Card.Header>Particle Masses / IBD Threshold</Card.Header>
       <Card.Body>
+        <Provider>
         <p>Mass values are from the Particle Data Group <a href="https://pdg.lbl.gov/2020/reviews/rpp2020-rev-phys-constants.pdf">at this link</a>.</p>
         <Table>
             <thead>
@@ -38,6 +41,9 @@ export const ParticleMasses = () => {
               </tr>
             </tbody>
           </Table>
+          <div> <p> blah balh blah </p>
+          </div>
+        </Provider>
       </Card.Body>
     </Card>
   );
