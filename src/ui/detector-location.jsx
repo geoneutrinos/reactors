74eeb3624b7e385c37d4fd4e98259a2c12abd489
Stephen Dye <47828035+stdye@users.noreply.github.com>
2021-11-30 12:14:14 -1000

    try limit decimal places- custom detector lat/lon

--- src/ui/detector-location.jsx
+++ src/ui/detector-location.jsx
@@ -106,7 +106,7 @@ export const DetectorLocationPane = ({ detector, setDetector }) => {
           <Form.Label>Latitude</Form.Label>
           <InputGroup>
             <Form.Control
-              value={internalDetector.lat}
+              value={internalDetector.lat.toFixed(5)}
               onChange={(event) => checkAndSet("lat", event.target.value)}
               type="number"
               placeholder="0"
@@ -121,7 +121,7 @@ export const DetectorLocationPane = ({ detector, setDetector }) => {
           <Form.Label>Longitude</Form.Label>
           <InputGroup>
             <Form.Control
-              value={internalDetector.lon}
+              value={internalDetector.lon.toFixed(5)}
               onChange={(event) => checkAndSet("lon", event.target.value)}
               type="number"
               placeholder="0"
