be363c3f9c832929531a35313accf4f81c5dcfa3
Stephen Dye <47828035+stdye@users.noreply.github.com>
2021-12-08 09:00:46 -1000

    Update detector-physics.jsx
    this is just an initial attempt... value definition of crossSection.elasticScatteringTmax likely to be needed other in other code files

--- src/ui/detector-physics.jsx
+++ src/ui/detector-physics.jsx
@@ -30,6 +30,28 @@ export const DetectorPhysicsPane = () => {
     </Form.Group>
   );
 
+  const TMaxRange = (
+    <Form.Group controlId="tmaxsrange">
+      <Form.Label>
+        Elastic Scattering T<sub>max</sub>:{" "}
+        {crossSection.elasticScatteringTMax.toFixed(1)} MeV
+      </Form.Label>
+      <Form.Control
+        type="range"
+        min={0}
+        max={7}
+        step={0.1}
+        value={crossSection.elasticScatteringTMax}
+        onChange={(event) =>{
+          crossSectionDispatch({
+            arg: "elasticScatteringTMax",
+            value: parseFloat(event.target.value),
+          });
+        }}
+      ></Form.Control>
+    </Form.Group>
+  );
+
   const CrossSectionInput = (
     <Form.Group controlId="neutrinoCrossSection">
       <Form.Label>Neutrino Cross Section</Form.Label>
