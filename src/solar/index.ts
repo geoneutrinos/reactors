e19fae9b2149b9610b4012c8782bb853cfe4a5f1
Stephen Dye <47828035+stdye@users.noreply.github.com>
2021-12-11 12:25:25 -1000

    Update index.ts

--- src/solar/index.ts
+++ src/solar/index.ts
@@ -33,7 +33,7 @@ const b8RateToKE = (b8Rate: number[], crossSection: CrossSection) => {
   const eVtoK = boron8Bins.map((bin) => {
     const Tspec = boron8Bins.map((Tbin) =>
       Tbin < esTmin
-        ? 0 ;                                 
+        ? 0 ,
       Tbin > esTmax
         ? 0
         : crossSectionElasticScattering(
