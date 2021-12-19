94e413c210d0bbafb348f21a060835f97f845984
Stephen Dye <47828035+stdye@users.noreply.github.com>
2021-12-18 15:14:58 -1000

    Update constants.ts
    this is for comparing reactor signal results with the new Kurchatov Institute 235U and 238U fission isotope spectra to those given by the existing deployment

--- src/physics/constants.ts
+++ src/physics/constants.ts
@@ -31,8 +31,8 @@ export type VFitIsotopes = {
 // Huber 2011 -> DOI: 10.1103/PhysRevC.84.024617
 // Mueller 2011 -> 10.1103/PhysRevC.83.054615
 export const V_FIT_PARAMS: VFitIsotopes = {
-  U235: [4.367, -4.577, 2.1, -5.294e-1, 6.186e-2, -2.777e-3], // Huber 2011 (phys rev c) table 3
-  U238: [4.833e-1, 1.927e-1, -1.283e-1, -6.762e-3, 2.233e-3, -1.536e-4], // Mueller 2011 table 6
+  U235: [4.353, -4.624, 2.121, -5.335e-1, 6.221e-2, -2.785e-3], // fit of data in Kopeikin 2021 (phys rev d) table 1
+  U238: [2.836, -2.777, 1.376, -3.776e-1, 4.557e-2, -2.072e-3], // fit of data in Kopeikin 2021 (phys rev d) table 1
   PU239: [4.757, -5.392, 2.563, -6.596e-1, 7.82e-2, -3.536e-3], // Huber 2011 (phys rev c) table 3
   PU241: [2.99, -2.882, 1.278, -3.343e-1, 3.905e-2, -1.754e-3], // Huber 2011 (phys rev c) table 3
 };
