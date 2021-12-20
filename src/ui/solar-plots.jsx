5e1a8041b7f8d8a325fdeebe8c65aa77032eba50
Stephen Dye <47828035+stdye@users.noreply.github.com>
2021-12-19 18:07:10 -1000

    Update solar-plots.jsx

--- src/ui/solar-plots.jsx
+++ src/ui/solar-plots.jsx
@@ -186,7 +186,7 @@ export const AnalemmaPlot = ({ detector, cores, reactorLF, boron8 }) => {
   };
   return (
     <Card>
-      <Card.Header><sup>8</sup>B Solar Neutrino Rate and Solar Analemma with Cores</Card.Header>
+      <Card.Header>Solar Neutrino Rates and Solar Analemma with Cores</Card.Header>
       <Card.Body>
          <Card.Text>
           <i>R</i><sub>boron-8</sub> = {boron8.boron8NIU.toFixed(2)} NIU ({crossSection.elasticScatteringTMin.toFixed(1)} &lt; T &lt; {crossSection.elasticScatteringTMax.toFixed(1)} MeV) 
