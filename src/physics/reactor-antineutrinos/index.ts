4db9694dab08d4a323c571aec2f4e09773cdeb2f
Andrew Barna <abarna@gmail.com>
2022-01-16 02:53:20 +0000

    Save WIP for runtime switching of reactor antineutrino models

--- src/physics/reactor-antineutrinos/index.ts
+++ src/physics/reactor-antineutrinos/index.ts
@@ -1,3 +1,5 @@
+import { IsotopeKeys } from "../constants";
+
 /**
  * Returns the partial internation rate which has not been scaled for 
  * thermal power of a core, had the neutrinos oscilated, or had the 
@@ -10,4 +12,17 @@
  */
 export function partialInteractionRate(Ev: number, Q: number, crossSection:(Ev:number)=>number, neutrinoEnergyFunction:(Ev: number) => number): number{
   return neutrinoEnergyFunction(Ev) * 1/Q * crossSection(Ev)/(4 * Math.PI);
+}
+
+type ReactorAntineutrinoModelFunc = (Ev: number) => number
+
+type ReactorAntineutrinoModel = Record<IsotopeKeys, ReactorAntineutrinoModelFunc>
+
+export enum RANames {
+  HM2011 = "Huber-Mueller 2011",
+  ES2018 = "Estienne et. al. 2018",
+}
+
+interface ReactorAntineutrinoModelConfig {
+  reactorAntineutrino: RANames
 }
