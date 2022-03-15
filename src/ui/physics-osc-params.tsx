dcab4707cb2380e0927de6f0d8be77d6cce2ce5c
Stephen Dye <47828035+stdye@users.noreply.github.com>
2022-03-14 21:04:30 -1000

    Update physics-osc-params.tsx

--- src/ui/physics-osc-params.tsx
+++ src/ui/physics-osc-params.tsx
@@ -71,10 +71,9 @@ export const PhysicsOscillationPane = () => {
           </tbody>
         </Table>
         <p>
+          NO: Normal mass ordering (m<sub>3</sub> {">"} m<sub>2</sub> {">"} m<sub>1</sub>)
           <br />
-          NO: Normal mass ordering (m<sub>3</sub> > m<sub>2</sub> > m<sub>1</sub>)
-          <br />
-          IO: Inverted mass ordering (m<sub>2</sub> > m<sub>1</sub> > m<sub>3</sub>)
+          IO: Inverted mass ordering (m<sub>2</sub> {">"} m<sub>1</sub> {">"} m<sub>3</sub>)
         </p>
       </Card.Body>
     </Card>
