20984ac6127cdfbf13b9929946d8362cdd3f6164
Andrew Barna <abarna@gmail.com>
2022-03-24 03:19:24 +0000

    Enable 2020 LF data in the web app

--- src/App.js
+++ src/App.js
@@ -117,8 +117,8 @@ function App(props) {
     ...defaultDetector,
   });
   const [reactorLF, setReactorLF] = useState({
-    start: new Date("2019-01-01T00:00:00Z"),
-    end: new Date("2019-12-01T00:00:00Z"),
+    start: new Date("2020-01-01T00:00:00Z"),
+    end: new Date("2020-12-01T00:00:00Z"),
   });
 
   const [coreMods, setCoreMods] = useState({});
