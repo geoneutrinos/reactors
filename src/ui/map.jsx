9fd6ee6423d004c0fd405e39f243bd587c2fd10e
Stephen Dye <47828035+stdye@users.noreply.github.com>
2022-03-30 08:49:12 -1000

    Update map.jsx

--- src/ui/map.jsx
+++ src/ui/map.jsx
@@ -182,6 +182,7 @@ export function NuMap({
       style={mapStyle}
       center={[0, 0]}
       zoom={2}
+      minZoom={1}
       {...contextMenu}
       onZoom={(e) => setZoom(e.target._zoom)}
     >
