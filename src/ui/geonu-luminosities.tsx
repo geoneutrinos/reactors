bf11b80d50c0154fb36254637be56074a8b91750
Stephen Dye <47828035+stdye@users.noreply.github.com>
2022-04-04 14:12:15 -1000

    Update geonu-luminosities.tsx

--- src/ui/geonu-luminosities.tsx
+++ src/ui/geonu-luminosities.tsx
@@ -10,7 +10,7 @@ const {K40, Th232, U235, U238} = Elements
 export const IsotopeNuLuminosities = () => {
   return (
     <Card>
-      <Card.Header>Isotopic Neutrino Luminosities- <small>kg<sup>-1</sup> µs<sup>-1</sup></small></Card.Header>
+      <Card.Header>Isotope Neutrino Luminosities- <small>kg<sup>-1</sup> µs<sup>-1</sup></small></Card.Header>
       <Card.Body>
         <Table>
             <thead>
