7bb4229be9b2a05ca3db326ad529469e68e55add
Stephen Dye <47828035+stdye@users.noreply.github.com>
2022-04-05 16:33:13 -1000

    Update derived.ts

--- src/physics/derived.ts
+++ src/physics/derived.ts
@@ -8,6 +8,7 @@ import {
   NEUTRON_REST_MASS,
   PROTON_REST_MASS,
   ALPHA_REST_MASS,
+  ELEMENTARY_CHARGE,
 } from "./constants";
 
 import Elements from "../elements";
@@ -21,13 +22,17 @@ const {K40, Th232, U235, U238, Ca40, Pb208, Pb207, Pb206} = Elements
 export const ISOTOPIC_DECAY_ENERGIES = {
    // MeV
    K40:
-     (K40.relative_atomic_mass - Ca40.relative_atomic_mass) - ISOTOPIC_ALPHAS_PER_DECAY.K40 * (ALPHA_REST_MASS + 2 * ELECTRON_REST_MASS),
+     ((K40.relative_atomic_mass - Ca40.relative_atomic_mass) - ISOTOPIC_ALPHAS_PER_DECAY.K40 * 
+      (ALPHA_REST_MASS + 2 * ELECTRON_REST_MASS)) * ELEMENTARY_CHARGE * 1e6,
    TH232:
-     (Th232.relative_atomic_mass - Pb208.relative_atomic_mass) - ISOTOPIC_ALPHAS_PER_DECAY.Th232 * (ALPHA_REST_MASS + 2 * ELECTRON_REST_MASS),
+     ((Th232.relative_atomic_mass - Pb208.relative_atomic_mass) - ISOTOPIC_ALPHAS_PER_DECAY.Th232 * 
+      (ALPHA_REST_MASS + 2 * ELECTRON_REST_MASS)) * ELEMENTARY_CHARGE * 1e6,
    U235:
-     (U235.relative_atomic_mass - Pb207.relative_atomic_mass) - ISOTOPIC_ALPHAS_PER_DECAY.U235 * (ALPHA_REST_MASS + 2 * ELECTRON_REST_MASS),
+     ((U235.relative_atomic_mass - Pb207.relative_atomic_mass) - ISOTOPIC_ALPHAS_PER_DECAY.U235 * 
+      (ALPHA_REST_MASS + 2 * ELECTRON_REST_MASS)) * ELEMENTARY_CHARGE * 1e6,
    U238:
-     (U238.relative_atomic_mass - Pb206.relative_atomic_mass) - ISOTOPIC_ALPHAS_PER_DECAY.U238 * (ALPHA_REST_MASS + 2 * ELECTRON_REST_MASS),
+     ((U238.relative_atomic_mass - Pb206.relative_atomic_mass) - ISOTOPIC_ALPHAS_PER_DECAY.U238 * 
+      (ALPHA_REST_MASS + 2 * ELECTRON_REST_MASS)) * ELEMENTARY_CHARGE * 1e6,
  };
 
 // ISOTOPIC_DECAY_HEATING moved to antineutrino-specturm due to circular import
