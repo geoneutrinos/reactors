041091e95d40fe393d910d177790943d2ebbd452
Stephen Dye <47828035+stdye@users.noreply.github.com>
2022-04-05 16:24:47 -1000

    Update derived.ts

--- src/physics/derived.ts
+++ src/physics/derived.ts
@@ -13,10 +13,22 @@ import {
 import Elements from "../elements";
 
 // Can "destructure" the elements needed:
-const {K40, Th232, U235, U238} = Elements
+const {K40, Th232, U235, U238, Ca40, Pb208, Pb207, Pb206} = Elements
 
 // These have a relative_atomic_mass property
 // e.g. K40.relative_atomic_mass is 39.963998166
+
+export const ISOTOPIC_DECAY_ENERGIES = {
+   // MeV
+   K40:
+     (K40.relative_atomic_mass - Ca40.relative_atomic_mass) - ISOTOPIC_ALPHAS_PER_DECAY.K40 * (ALPHA_REST_MASS + 2 * ELECTRON_REST_MASS),
+   TH232:
+     (Th232.relative_atomic_mass - Pb208.relative_atomic_mass) - ISOTOPIC_ALPHAS_PER_DECAY.Th232 * (ALPHA_REST_MASS + 2 * ELECTRON_REST_MASS),
+   U235:
+     (U235.relative_atomic_mass - Pb207.relative_atomic_mass) - ISOTOPIC_ALPHAS_PER_DECAY.U235 * (ALPHA_REST_MASS + 2 * ELECTRON_REST_MASS),
+   U238:
+     (U238.relative_atomic_mass - Pb206.relative_atomic_mass) - ISOTOPIC_ALPHAS_PER_DECAY.U238 * (ALPHA_REST_MASS + 2 * ELECTRON_REST_MASS),
+ };
 
 // ISOTOPIC_DECAY_HEATING moved to antineutrino-specturm due to circular import
   
