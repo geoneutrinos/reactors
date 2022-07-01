c68d1224df4b269e197e4076be53890d872bd0ce
Stephen Dye <47828035+stdye@users.noreply.github.com>
2022-07-01 13:24:19 -1000

    Update isotope-data.tsx

--- src/ui/isotope-data.tsx
+++ src/ui/isotope-data.tsx
@@ -104,8 +104,8 @@ export const IsotopeData = () => {
               </tr>
               <tr>
                 <td> Half life (10<sup>9</sup> y) </td>
-                 <td colSpan={2}> {" ----- "}
-                  {ISOTOPIC_HALF_LIFE.K40e9y} {" ----- "}
+                 <td colSpan={2} style={{ textAlign: 'center' }}>
+                  {ISOTOPIC_HALF_LIFE.K40e9y}
                 </td>
                 <td>
                   {ISOTOPIC_HALF_LIFE.TH232e9y.toFixed(1)}
@@ -119,8 +119,8 @@ export const IsotopeData = () => {
               </tr>
               <tr>
                 <td> Natural abundance (%) </td>
-                 <td colSpan={2}> {" ----- "}
-                  {ISOTOPIC_NATURAL_ABUNDANCE.K40} {" ----- "}
+                 <td colSpan={2} style={{ textAlign: 'center' }}>
+                  {ISOTOPIC_NATURAL_ABUNDANCE.K40}
                 </td>
                 <td>
                   {ISOTOPIC_NATURAL_ABUNDANCE.TH232}
