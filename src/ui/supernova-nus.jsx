bf45b55a13f6362620ef7f482bdf47c22035fc97
Stephen Dye <47828035+stdye@users.noreply.github.com>
2022-07-04 10:06:46 -1000

    Update supernova-nus.jsx

--- src/ui/supernova-nus.jsx
+++ src/ui/supernova-nus.jsx
@@ -13,7 +13,7 @@ export const SupernovaNusPane = () => {
           <div>
             <p>
               <Node>{String.raw`
-                F_\nu(E_\nu)=\frac1{4 \pi D^2} \frac{\Phi_\nu}{\langle E_\nu \rangle} \frac{\beta^\beta}{\Gamma(\beta)}[\frac{E}{\langle E_\nu \rangle}]^{\beta-1} \mathrm{exp}[-\beta \frac{E}{\langle E_\nu \rangle}]
+                F_\nu(E_\nu)=\frac1{4 \pi D^2} \frac{\Phi_\nu}{\langle E_\nu \rangle} \frac{\beta^\beta}{\Gamma(\beta)}\big[\frac{E}{\langle E_\nu \rangle}\big]^{\beta-1} \mathrm{exp}\big[-\beta \frac{E}{\langle E_\nu \rangle}\big]
                 `}</Node> where {" "}
               <Node inline>{String.raw`D`}</Node> is the distance to the SN.
             </p>
