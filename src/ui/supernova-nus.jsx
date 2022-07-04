cc16700dd84f5dcb5feaa0f6a8cbbf73449d0cf5
Stephen Dye <47828035+stdye@users.noreply.github.com>
2022-07-04 10:22:28 -1000

    Update supernova-nus.jsx

--- src/ui/supernova-nus.jsx
+++ src/ui/supernova-nus.jsx
@@ -13,9 +13,11 @@ export const SupernovaNusPane = () => {
           <div>
             <p>
               <Node>{String.raw`
-                F_\nu(E_\nu)=\frac1{4 \pi D^2} \frac{\Phi_\nu}{\langle E_\nu \rangle} \frac{\beta^\beta}{\Gamma(\beta)}\big[\frac{E}{\langle E_\nu \rangle}\big]^{\beta-1} \mathrm{exp}\big[-\beta \frac{E}{\langle E_\nu \rangle}\big]
+                F_\nu(E)=\frac1{4 \pi D^2} \frac{\Phi_\nu}{\langle E_\nu \rangle} \frac{\beta^\beta}{\Gamma(\beta)}\bigg[\frac{E}{\langle E_\nu \rangle}\bigg]^{\beta-1} \mathrm{exp}\bigg[-\beta \frac{E}{\langle E_\nu \rangle}\bigg]
                 `}</Node> where {" "}
-              <Node inline>{String.raw`D`}</Node> is the distance to the SN.
+              <Node inline>{String.raw`D`}</Node> is the distance to the SN. <Node inline>{String.raw`\Phi_\nu`}</Node> is the number of neutrinos, 
+              <Node inline>{String.raw`\beta`}</Node> is a parameter, <Node inline>{String.raw`\Gamma`}</Node> is the gamma function.
+              
             </p>
           </div>
         </Provider>
