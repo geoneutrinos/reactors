577dd5f1710baba6cc3914654c9ada297b59e07c
Stephen Dye <47828035+stdye@users.noreply.github.com>
2022-07-05 13:16:34 -1000

    Update supernova-nus.jsx

--- src/ui/supernova-nus.jsx
+++ src/ui/supernova-nus.jsx
@@ -19,12 +19,12 @@ export const SupernovaNusPane = () => {
                 `}</Node> where {" "}
               <Node inline>{String.raw`D`}</Node> is the distance to the SN, <Node inline>{String.raw`E_{\nu_{\alpha}}^\mathrm{tot}`}</Node> is the total energy of the neutrinos, {" "}
               <Node inline>{String.raw`\beta`}</Node> is a parameter, <Node inline>{String.raw`\Gamma`}</Node> is the gamma function, {" "}
-              and <Node inline>{String.raw`\langle E_\nu \rangle`}</Node> is the average neutrino energy. 
+              and <Node inline>{String.raw`\langle E_{\nu{\alpha}} \rangle`}</Node> is the average neutrino energy. 
             </p>
             <p>
               Initially assume {" "}
               <Node inline>{String.raw`D = 10`}</Node> kpc, {" "}
-              <Node inline>{String.raw`E_\nu^\mathrm{tot} = 5\times10^{52}`}</Node> erg, {" "}
+              <Node inline>{String.raw`E_{\nu{\alpha}}^\mathrm{tot} = 5\times10^{52}`}</Node> erg, {" "}
               <Node inline>{String.raw`\beta = 4`}</Node> , {" "}
               <Node inline>{String.raw`\langle E_{\nu_{\mathrm{e}}} \rangle = 12`}</Node> MeV, {" "} 
               <Node inline>{String.raw`\langle E_{\overline{\nu}_{\mathrm{e}}} \rangle = 15`}</Node> MeV, and {" "}
@@ -42,7 +42,7 @@ export const SupernovaNusPane = () => {
               For IO {" "}
               <Node>{String.raw`\begin{aligned}
                 & F_{\nu_{\mathrm{e}}}(E) = F^0_{\nu_{\mathrm{e}}}(E)\sin^2\theta_{12} + F^0_{\nu_{x}}(E)\cos^2\theta_{12} \\
-                & F_{\overline{\nu}_{\mathrm{e}}}(E) = F^0_{\nu_{x}}(E)\cos^2\theta_{12} \\
+                & F_{\overline{\nu}_{\mathrm{e}}}(E) = F^0_{\nu_{x}}(E) \\
                 & F_{\nu_{x}}(E) = \frac1{4}(2+\sin^2\theta_{12})F^0_{\nu_{x}}(E) + \frac1{4}F^0_{\overline{\nu}_{\mathrm{e}}}(E) + \frac1{4}F^0_{\overline{\nu}_{\mathrm{e}}}(E)\cos^2\theta_{12}
                 \end{aligned}`}</Node>
               </p>
