892ef13b3f7365d8529f6be23da668f0a52d7309
Stephen Dye <47828035+stdye@users.noreply.github.com>
2022-07-05 14:56:05 -1000

    Update supernova-nus.jsx

--- src/ui/supernova-nus.jsx
+++ src/ui/supernova-nus.jsx
@@ -15,7 +15,7 @@ export const SupernovaNusPane = () => {
               In the absence of oscillations, the estimated energy spectrum (/area/energy) of core collapse supernova neutrinos of a given species {" "}
               <Node inline>{String.raw`(\nu_\mathrm{e}, \overline{\nu}_\mathrm{e}, \nu_x)`}</Node> arriving at a detection site is
               <Node>{String.raw`
-                F^0_{\nu_{\alpha}}(E)=\frac1{4 \pi D^2} \frac{E_{\nu_{\alpha}}^\mathrm{tot}}{\langle E_{\nu_\alpha} \rangle^2} \frac{\beta^\beta}{\Gamma(\beta)}\bigg[\frac{E}{\langle E_{\nu_\alpha} \rangle}\bigg]^{\beta-1} \mathrm{exp}\bigg[-\beta \frac{E}{\langle E_{\nu_\alpha} \rangle}\bigg]
+                \Phi^0_{\nu_{\alpha}}(E)=\frac1{4 \pi D^2} \frac{E_{\nu_{\alpha}}^\mathrm{tot}}{\langle E_{\nu_\alpha} \rangle^2} \frac{\beta^\beta}{\Gamma(\beta)}\bigg[\frac{E}{\langle E_{\nu_\alpha} \rangle}\bigg]^{\beta-1} \mathrm{exp}\bigg[-\beta \frac{E}{\langle E_{\nu_\alpha} \rangle}\bigg]
                 `}</Node> where {" "}
               <Node inline>{String.raw`D`}</Node> is the distance to the SN, <Node inline>{String.raw`E_{\nu_{\alpha}}^\mathrm{tot}`}</Node> is the total energy of the neutrinos, {" "}
               <Node inline>{String.raw`\beta`}</Node> is a parameter, <Node inline>{String.raw`\Gamma`}</Node> is the gamma function, {" "}
@@ -33,17 +33,17 @@ export const SupernovaNusPane = () => {
             <p>
               Oscillation effects depend on the neutrino mass ordering (NO or IO). For NO {" "}
               <Node>{String.raw`\begin{aligned}
-                & F_{\nu_{\mathrm{e}}}(E) = F^0_{\nu_{x}}(E) \\
-                & F_{\overline{\nu}_{\mathrm{e}}}(E) = F^0_{\overline{\nu}_{\mathrm{e}}}(E)\cos^2\theta_{12} + F^0_{\nu_{x}}(E)\sin^2\theta_{12} \\
-                & F_{\nu_{x}}(E) = \frac1{4}(F^0_{\nu_{x}}(E)(2+\cos^2\theta_{12}) + F^0_{\nu_{\mathrm{e}}}(E) + F^0_{\overline{\nu}_{\mathrm{e}}}(E)\sin^2\theta_{12})
+                & \Phi_{\nu_{\mathrm{e}}} = \Phi^0_{\nu_{x}} \\
+                & \Phi_{\overline{\nu}_{\mathrm{e}}} = \Phi^0_{\overline{\nu}_{\mathrm{e}}}\cos^2\theta_{12} + \Phi^0_{\nu_{x}}\sin^2\theta_{12} \\
+                & \Phi_{\nu_{x}} = \frac1{4}(\Phi^0_{\nu_{x}}(2+\cos^2\theta_{12}) + \Phi^0_{\nu_{\mathrm{e}}} + \Phi^0_{\overline{\nu}_{\mathrm{e}}}\sin^2\theta_{12})
                 \end{aligned}`}</Node>
               </p>
               <p>
               For IO {" "}
               <Node>{String.raw`\begin{aligned}
-                & F_{\nu_{\mathrm{e}}}(E) = F^0_{\nu_{\mathrm{e}}}(E)\sin^2\theta_{12} + F^0_{\nu_{x}}(E)\cos^2\theta_{12} \\
-                & F_{\overline{\nu}_{\mathrm{e}}}(E) = F^0_{\nu_{x}}(E) \\
-                & F_{\nu_{x}}(E) = \frac1{4}(F^0_{\nu_{x}}(E)(2+\sin^2\theta_{12}) + F^0_{\overline{\nu}_{\mathrm{e}}}(E) + F^0_{\nu_{\mathrm{e}}}(E)\cos^2\theta_{12})
+                & \Phi_{\nu_{\mathrm{e}}} = \Phi^0_{\nu_{\mathrm{e}}}\sin^2\theta_{12} + \Phi^0_{\nu_{x}}\cos^2\theta_{12} \\
+                & \Phi_{\overline{\nu}_{\mathrm{e}}} = \Phi^0_{\nu_{x}} \\
+                & \Phi_{\nu_{x}} = \frac1{4}(\Phi^0_{\nu_{x}}(2+\sin^2\theta_{12}) + \Phi^0_{\overline{\nu}_{\mathrm{e}}} + \Phi^0_{\nu_{\mathrm{e}}}\cos^2\theta_{12})
                 \end{aligned}`}</Node>
               </p>
           </div>
