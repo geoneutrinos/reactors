3c27e53b67bb0e19ba28ee82ddfc836cf476b991
Stephen Dye <47828035+stdye@users.noreply.github.com>
2022-07-07 08:29:43 -1000

    simply another space...

--- src/ui/reactors-core-iaea-select.jsx
+++ src/ui/reactors-core-iaea-select.jsx
@@ -98,7 +98,7 @@ export const CoreIAEARange = ({
         <p>
           <br />
           Monthly load factor (LF) data for the years starting with 2020 are from 
-          <a href="https://pris.iaea.org/pris/home.aspx/"> IAEA - Power Reactor Information System (PRIS)</a> 
+          <a href="https://pris.iaea.org/pris/home.aspx/"> IAEA - Power Reactor Information System (PRIS) </a> 
           and for the earlier years 2003 to 2019 from 
           <a href="https://www.fe.infn.it/antineutrino/"> INFN Antineutrinos</a>. If
           selecting the "Use IAEA LF Data" (default) option,
