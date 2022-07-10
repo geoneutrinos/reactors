9e8d26249fa94b44a85ee3fda8cfc011cb7fd2b0
Stephen Dye <47828035+stdye@users.noreply.github.com>
2022-07-09 15:09:14 -1000

    adding code for spectrums

--- src/supernova/index.ts
+++ src/supernova/index.ts
@@ -1,12 +1,48 @@
-import { ELEMENTARY_CHARGE } from "../physics/constants";
-import { crossSectionSV2003 } from "../physics/neutrino-cross-section";
-import { memoize } from 'lodash';
-import bins, {binWidth, binCount} from "../physics/bins";
+import { ELEMENTARY_CHARGE, ELECTRON_REST_MASS } from "../physics/constants";
+import { IBD_THRESHOLD } from "../physics/derived";
 
-function NuSpecCCSN(Ev:number): number{
-  const enu_tot = 5e52 * 1e-13 / ELEMENTARY_CHARGE;  // MeV
+var nueSpectrum = [];
+var anuSpectrum = [];
+var nuxSpectrum = [];
+var xsecSV03 = [];
+var anuEventSpectrum = [];
+
+var sumEvents = 0;
+
+const deltaE = 0.1;
+
+for (let i=0; i<1000; i++) {
+  energyNu = i * deltaE + deltaE / 2; // MeV
+  nueSpectrum.push(nuSpecCCSN(energyNu,4,12));
+  anuSpectrum.push(nuSpecCCSN(energyNu,4,15));
+  nuxSpectrum.push(nuSpecCCSN(energyNu,4,18));
+  xsecSV03.push(xSection(energyNu));
+  anuEventSpectrum.push((anuSpectrum(i) * xsecSV03(i) * 1e32 );
+  sumEvents = sumEvents + anuEventSpectrum(i) * deltaE;
+}
+
+function nuSpecCCSN(Ev,beta,avgE): number{
+  const enu_tot = 5e52 * 1e-13 / ELEMENTARY_CHARGE; // MeV
   const d_ccsn = 10 * 3.086e21; // cm
-  const prefix = (4 ** 4) / ( 4 * Math.PI * 6 * 15 * 15 );
-  const energy_factor = ( ( Ev / 15 ) ** 3 ) * Math.exp(Ev * 4 / 15);
-  return prefix*enu_tot*energy_factor/d_ccsn/d_ccsn
+
+  const prefix = (beta ** beta) / ( 4 * Math.PI * 6 * avgE * avgE );
+
+  const energy_factor = ( ( Ev / avgE ) ** (beta - 1) ) * Math.exp(-beta * Ev / avgE);
+
+  return prefix * enu_tot * energy_factor / d_ccsn / d_ccsn
+}
+
+function xSection(Ev): number{
+  const a = -0.07056;
+  const b = 0.02018;
+  const c = -0.001953;
+
+  const sv = a + (b * Math.log(Ev)) + (c * Math.log(Ev) ** 3);
+  const sve = Ev ** sv
+  
+  const Ee = Math.max(ELECTRON_REST_MASS, Ev - IBD_THRESHOLD + ELECTRON_REST_MASS)
+  
+  const Pe = Math.sqrt(Ee ** 2 - ELECTRON_REST_MASS ** 2) // electron momentum
+
+  return 1e-43 * Pe * Ee * sve;
 }
