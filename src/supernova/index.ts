06918842cbf0d79d16fc3ac243644a150e303017
Stephen Dye <47828035+stdye@users.noreply.github.com>
2022-07-12 12:03:50 -1000

    Update index.ts

--- src/supernova/index.ts
+++ src/supernova/index.ts
@@ -5,14 +5,42 @@ export const energyValues = new Float32Array(1000).map((v, i) => i / 10 + .05);
 
 const xsection = energyValues.map(xSection);
 
-// todo: repeat this for nue, anu, nux with different avgE
-export const fluxSpectrum = energyValues.map(nuSpecCCSN);
+// todo: pass avgE as parameter to single function that still maps 
+export const fluxSpectrumNue = energyValues.map(nueSpecCCSN);
+export const fluxSpectrumAnu = energyValues.map(anuSpecCCSN);
+export const fluxSpectrumNux = energyValues.map(nuxSpecCCSN);
 
-function nuSpecCCSN(Ev: number) {
+function nueSpecCCSN(Ev: number) {
+  const enu_tot = 5e52 * 1e-13 / ELEMENTARY_CHARGE; // MeV
+  const d_ccsn = 10 * 3.086e21; // cm
+  const beta = 4;
+  const avgE = 12;
+
+  const prefix = (beta ** beta) / ( 4 * Math.PI * 6 * avgE * avgE );
+
+  const energy_factor = ( ( Ev / avgE ) ** (beta - 1) ) * Math.exp(-beta * Ev / avgE);
+
+  return prefix * enu_tot * energy_factor / d_ccsn / d_ccsn;
+}
+
+function anuSpecCCSN(Ev: number) {
   const enu_tot = 5e52 * 1e-13 / ELEMENTARY_CHARGE; // MeV
   const d_ccsn = 10 * 3.086e21; // cm
   const beta = 4;
   const avgE = 15;
+
+  const prefix = (beta ** beta) / ( 4 * Math.PI * 6 * avgE * avgE );
+
+  const energy_factor = ( ( Ev / avgE ) ** (beta - 1) ) * Math.exp(-beta * Ev / avgE);
+
+  return prefix * enu_tot * energy_factor / d_ccsn / d_ccsn;
+}
+
+function nuxSpecCCSN(Ev: number) {
+  const enu_tot = 5e52 * 1e-13 / ELEMENTARY_CHARGE; // MeV
+  const d_ccsn = 10 * 3.086e21; // cm
+  const beta = 4;
+  const avgE = 18;
 
   const prefix = (beta ** beta) / ( 4 * Math.PI * 6 * avgE * avgE );
 
