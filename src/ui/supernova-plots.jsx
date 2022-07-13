5e15b3dedcb10f7b98c759169e364434179c40ec
Stephen Dye <47828035+stdye@users.noreply.github.com>
2022-07-12 14:12:55 -1000

    Update supernova-plots.jsx

--- src/ui/supernova-plots.jsx
+++ src/ui/supernova-plots.jsx
@@ -43,7 +43,7 @@ export const SupernovaFluxPlots = () => {
     },
   ];
     var layout = {
-    title: "Core Collapse SN Neutrino Flux Spectra",
+    title: "Core Collapse SN Neutrino Flux Spectra w/o oscillations",
     yaxis: {
       title: { text: `Flux (/cm<sup>2</sup>/MeV)` },
       autorange: true,
@@ -71,7 +71,88 @@ export const SupernovaFluxPlots = () => {
   };
   return (
     <Card>
-      <Card.Header>Core Collapse SN Neutrino Flux Spectra</Card.Header>
+      <Card.Header>Core Collapse SN Neutrino Flux Spectra without oscillations</Card.Header>
+      <Card.Body>
+        <Plot
+          useResizeHandler={true}
+          style={{ width: "100%" }}
+          data={data}
+          layout={layout}
+          config={config}
+        />
+      </Card.Body>
+    </Card>
+  );
+};
+export const SupernovaOscillatedFluxPlots = () => {
+  const data = [
+    {
+      y: fluxSpectrumNue,
+      x: energyValues,
+      name: `ν<sub>e</sub>`,
+      type: "scatter",
+      mode: "lines",
+      line: {
+        width: 2
+      },
+      fill: "none",
+      marker: { color: "blue" },
+    },
+    {
+      y: fluxSpectrumAnu,
+      x: energyValues,
+      name: `ν̅<sub>e</sub>`,
+      type: "scatter",
+      mode: "lines",
+      line: {
+        width: 2
+      },
+      fill: "none",
+      marker: { color: "red" },
+    },
+    {
+      y: fluxSpectrumNux,
+      x: energyValues,
+      name: `ν<sub>x</sub>`,
+      type: "scatter",
+      mode: "lines",
+      line: {
+        width: 2
+      },
+      fill: "none",
+      marker: { color: "green" },
+    },
+  ];
+    var layout = {
+    title: "Core Collapse SN Neutrino Flux Spectra w/ oscillations",
+    yaxis: {
+      title: { text: `Flux (/cm<sup>2</sup>/MeV)` },
+      autorange: true,
+    },
+    xaxis: {
+      title: { text: `Neutrino Energy (MeV)` },
+      range: [0.05, 60.05],
+    },
+    autosize: true,
+    annotations: [
+      {
+        showarrow: false,
+        text: "geoneutrinos.org",
+        x: 1.1,
+        xref: "paper",
+        y: -0.15,
+        yref: "paper",
+      },
+    ],
+  };
+  var config = {
+    toImageButtonOptions: {
+      filename: 'Supernova-Oscillated-Flux-Spectra'
+    }
+  };
+  return (
+    <Card>
+      <Card.Header>Core Collapse SN Neutrino Flux Spectra with oscillations</Card.Header>
       <Card.Body>
         <Plot
           useResizeHandler={true}
