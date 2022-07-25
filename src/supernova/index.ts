ea469b5e6246d56d5bdd6c615e0417f44db9ddc4
Stephen Dye <47828035+stdye@users.noreply.github.com>
2022-07-25 10:08:35 -1000

    Update index.ts

--- src/supernova/index.ts
+++ src/supernova/index.ts
@@ -20,6 +20,8 @@ import { s2t12, c2t12 } from "../physics/neutrino-oscillation";
 
 import { sum } from "lodash";
 
+import Elements from "../elements";
+
 const neutrinoTargets = 1e32; // for IBD, eES, pES
 
 // ToDo make UI to select CEvNS target
