b974273f98818792d138ab8c22c7a09a0898dd36
Stephen Dye <47828035+stdye@users.noreply.github.com>
2022-08-16 09:38:04 -1000

    Update index.tsx

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -19,6 +19,7 @@ export { PhysicsOscillationPane } from "./physics-osc-params";
 export { PhysicsConstants } from "./physics-constants";
 export { ParticleMasses } from "./physics-masses";
 export { GeoFluxUncertainties } from "./geo-uncertainties";
+export { GeoRateFluxYields } from ".geo-ratefluxyields";
 export {IsotopeData } from "./isotope-data";
 export {
   CrossSectionPlots,
