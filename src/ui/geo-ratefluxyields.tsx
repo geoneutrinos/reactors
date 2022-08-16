833b8431022038402e649b6c6307f97b2dbbfcd7
Stephen Dye <47828035+stdye@users.noreply.github.com>
2022-08-16 09:28:32 -1000

    Create geo-ratefluxyields.tsx

---
+++ src/ui/geo-ratefluxyields.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+
+import { Card, Table } from 'react-bootstrap';
+
+import {Elements} from './elements'
+import {Num} from "."
+
+import {
+  rateToFlux232Th,
+  rateToFlux238U,
+  rateToFlux40K,
+} from "../antineutrino-spectrum";
+
+import { XSNames } from "../physics/neutrino-cross-section";
+
+const {K40, Th232, U235, U238} = Elements
+
+export const GeoRateFluxYields = () => {
+  return (
+    <Card>
+      <Card.Header>Geoneutrino Isotopic Rate to Flux</Card.Header>
+      <Card.Body>
+        <Table>
+            <thead>
+              <tr>
+                <th></th>
+                <th>{K40}<sub>β</sub></th>
+                <th>{Th232}</th>
+                <th>{U235}</th>
+                <th>{U238}</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Inverse Beta Decay (IBD)</td>
+                <td>
+                  
+                </td>
+                <td>
+                  
+                </td>
+                <td>
+                  
+                </td>
+                <td>
+                  
+                </td>
+              </tr>
+               <tr>
+                <td>Electron Elastic Scattering (eES)</td>
+                <td>
+                  
+                </td>
+                <td>
+                  
+                </td>
+                <td>
+                  
+                </td>
+                <td>
+                  
+                </td>
+              </tr>
+          </tbody>
+          </Table>
+        <p><small>
+          These factors convert isotopic geo-neutrino rates to fluxes<br />
+        </small></p>
+      </Card.Body>
+    </Card>
+  );
+}
