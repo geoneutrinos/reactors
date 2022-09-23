d9914603f43b8f0616582f32c72f258086ab13fc
Andrew Barna <abarna@gmail.com>
2022-09-23 02:52:58 +0000

    Do 16O interpolation in log10 input space

--- src/physics/oxygen-16/index.ts
+++ src/physics/oxygen-16/index.ts
@@ -1,18 +1,20 @@
 import { piecewise, scaleLinear } from "d3";
 import nuebar from "./nuebarIBDxsec.json";
-import nue from "./nueIBDxsec.json"
+import nue from "./nueIBDxsec.json";
 
-export const crossSection16OElectronNeutrino = (Ev: number):number => {
- const scale = scaleLinear()
-   .domain([nue.energy[0], nue.energy[nue.energy.length - 1]])
-   .clamp(true);
- const interpolator = piecewise(nue.crossSection)
- return interpolator(scale(Ev))
-}
-export const crossSection16OElectronAntineutrino = (Ev:number):number => {
- const scale = scaleLinear()
-   .domain([nuebar.energy[0], nuebar.energy[nue.energy.length - 1]])
-   .clamp(true);
- const interpolator = piecewise(nuebar.crossSection)
- return interpolator(scale(Ev))
-}
+export const crossSection16OElectronNeutrino = (Ev: number): number => {
+  const energy = nue.energy.map(Math.log10);
+  const scale = scaleLinear()
+    .domain([energy[0], energy[energy.length - 1]])
+    .clamp(true);
+  const interpolator = piecewise(nue.crossSection);
+  return interpolator(scale(Math.log10(Ev)));
+};
+export const crossSection16OElectronAntineutrino = (Ev: number): number => {
+  const energy = nuebar.energy.map(Math.log10);
+  const scale = scaleLinear()
+    .domain([energy[0], energy[energy.length - 1]])
+    .clamp(true);
+  const interpolator = piecewise(nuebar.crossSection);
+  return interpolator(scale(Math.log10(Ev)));
+};
