4210d1c32ffc9750650ab204adf939b846e6829a
Stephen Dye <47828035+stdye@users.noreply.github.com>
2022-09-27 15:32:32 -1000

    specify y-range on sec plot

--- src/ui/supernova-plots.jsx
+++ src/ui/supernova-plots.jsx
@@ -674,7 +674,7 @@ export const NeutrinoElectronElasticScatteringCrossSection = ({
     yaxis: {
       title: { text: `Cross Section (cm<sup>2</sup>)` },
       type: "log",
-      autorange: true,
+      range: [-45.,-38.6],
     },
     xaxis: {
       title: { text: `Neutrino Energy (MeV)` },
