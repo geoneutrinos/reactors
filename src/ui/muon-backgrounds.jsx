bd8cae2d97fc589985fbdd4f32cfc4b739364389
Stephen Dye <47828035+stdye@users.noreply.github.com>
2022-10-18 11:27:38 -1000

    start adding content to muon tab

--- src/ui/muon-backgrounds.jsx
+++ src/ui/muon-backgrounds.jsx
@@ -1,1 +1,31 @@
-const dummy = 2 * Math.Pi;
+import React, {useState, useMemo, memo} from "react";
+import { Card, Form, InputGroup, Table } from "react-bootstrap";
+
+import { Num } from ".";
+
+import { Node, Provider } from "@nteract/mathjax";
+
+const MuonsPane = () => {
+  return (
+    <Card>
+      <Card.Header>Muon-Induced Backgrounds</Card.Header>
+      <Card.Body>
+        <Provider>
+          <div>
+            <p>
+              This pane describes the overburden-dependent muon-induced backgrunds{" "}
+            </p>
+          </div>
+        </Provider>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export const Muons = React.memo(() => {
+  return (
+    <div>
+      <MuonsPane />
+    </div>
+  )
+})
