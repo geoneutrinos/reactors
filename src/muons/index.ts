60a6d1c65c6e9e40cc273958223c18bfc56bd80e
Stephen Dye <47828035+stdye@users.noreply.github.com>
2022-10-19 09:25:59 -1000

    add "//" for comment

--- src/muons/index.ts
+++ src/muons/index.ts
@@ -1,7 +1,7 @@
 // overburden-dependent muon-induced backgrouds
 
 const depthBins = 1000;
-const maximumDepth = 10; km.w.e.
+const maximumDepth = 10; // km.w.e.
 const deltaDepth = maximumDepth / depthBins; // km.w.e.
 
 // make the array of overburdens 0 - 10 km.w.e.
