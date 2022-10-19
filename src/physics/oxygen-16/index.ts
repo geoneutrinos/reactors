4efae0e7a646cc881749838df92962c5cbce8928
Stephen Dye <47828035+stdye@users.noreply.github.com>
2022-10-18 16:26:25 -1000

    tune E_mins in index.ts

--- src/physics/oxygen-16/index.ts
+++ src/physics/oxygen-16/index.ts
@@ -37,17 +37,17 @@ export const electronAntineutrino16OThresholdEnergy = 11.23
 export const electronNeutrino16OThresholdEnergy = 15.21
 
 const electronNeutrino16OFitParams = [ // Table 4
-  {Ex: 15.21, a:-40.008, b:4.918, c:1.036, Emin:15.345},
-  {Ex: 22.47, a:-39.305, b:4.343, c:0.961, Emin:22.695},
-  {Ex: 25.51, a:-39.655, b:5.263, c:1.236, Emin:25.845},
-  {Ex: 29.35, a:-39.166, b:3.947, c:0.901, Emin:29.675},
+  {Ex: 15.21, a:-40.008, b:4.918, c:1.036, Emin:15.35},
+  {Ex: 22.47, a:-39.305, b:4.343, c:0.961, Emin:22.71},
+  {Ex: 25.51, a:-39.655, b:5.263, c:1.236, Emin:25.88},
+  {Ex: 29.35, a:-39.166, b:3.947, c:0.901, Emin:29.72},
 ]
 
 const electronAntineutrino16OFitParams = [ // Table 4
-  {Ex: 11.23, a:-40.656, b:4.528, c:0.887, Emin:11.295},
-  {Ex: 18.50, a:-40.026, b:4.117, c:0.895, Emin:18.675},
-  {Ex: 21.54, a:-40.060, b:3.743, c:0.565, Emin:21.565},
-  {Ex: 25.38, a:-39.862, b:3.636, c:0.846, Emin:25.705},
+  {Ex: 11.23, a:-40.656, b:4.528, c:0.887, Emin:11.30},
+  {Ex: 18.50, a:-40.026, b:4.117, c:0.895, Emin:18.68},
+  {Ex: 21.54, a:-40.060, b:3.743, c:0.565, Emin:21.57},
+  {Ex: 25.38, a:-39.862, b:3.636, c:0.846, Emin:25.73},
 ]
 
 const partial16OCrossSection = (Ev: number, {Ex,a,b,c,Emin}:{Ex:number, a:number, b:number, c:number, Emin:number}): number => {
