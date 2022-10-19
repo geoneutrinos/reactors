c3244d81cfb7ac624f04e0b3216d002ba497ebe7
Stephen Dye <47828035+stdye@users.noreply.github.com>
2022-10-19 10:28:44 -1000

    add flat overburden muon flux & neutron flux fns

--- src/muons/index.ts
+++ src/muons/index.ts
@@ -9,13 +9,30 @@ export const depthValues = new Float64Array(depthBins).map(
   (v, i) => i * deltaDepth + deltaDepth / 2
 );
 
-// Muon Depth Intensity Relationship (DIR) Eq. 1 in Mei and Hime, PRD 73 (2006) 053004
-function muonIntensity(depth: number) {
-  const intensityParameter1 = 8.60e-6; // /cm^2/s/sr
-  const intensityParameter2 = 0.44e-6; // /cm^2/s/sr
-  const overburdenCoefficient1 = 0.45; // km.w.e.
-  const overburdenCoefficient2 = 0.87; // km.w.e.
+// differential muon intensity corresponding to slant depth- Eq. 1 in Mei and Hime, PRD 73 (2006) 053004
+function differentialMuonIntensity(slantDepth: number) {
+  const intensityParameter1a = 8.60e-6; // /cm^2/s/sr
+  const intensityParameter1b = 0.44e-6; // /cm^2/s/sr
+  const overburdenCoefficient1a = 0.45; // km.w.e.
+  const overburdenCoefficient1b = 0.87; // km.w.e.
     
-  return (intensityParameter1 * Math.exp(-depth / overburdenCoefficient1) + intensityParameter2 * Math.exp(-depth / overburdenCoefficient2));
+  return (intensityParameter1a * Math.exp(-slantDepth / overburdenCoefficient1a) + intensityParameter1b * Math.exp(-slantDepth / overburdenCoefficient1b));
 }
-  
+
+// differential muon intensity corresponding to flat overburden- Eq. 4 in Mei and Hime, PRD 73 (2006) 053004
+function flatOverburdenMuonIntensity(equivalentDepth: number) {
+  const intensityParameter4a = 69.97e-6; // /cm^2/s
+  const intensityParameter4b = 2.071e-6; // /cm^2/s
+  const overburdenCoefficient4a = 0.285; // km.w.e.
+  const overburdenCoefficient4b = 0.698; // km.w.e.
+    
+  return (intensityParameter4a * Math.exp(-equivalentDepth / overburdenCoefficient4a) + intensityParameter4b * Math.exp(-equivalentDepth / overburdenCoefficient4b));
+}
+
+// muon-induced neutron flux emerging from rock into cavern- Eq. 13 in Mei and Hime, PRD 73 (2006) 053004
+function neutronInducedFlux(flatDepth: number) {
+  const intensityParameter13 = 4.0e-7; // /cm^2/s
+  const overburdenCoefficient13 = 0.86; // km.w.e.
+    
+  return (intensityParameter13 * (overburdenCoefficient13 / flatDepth) * Math.exp(-flatDepth / overburdenCoefficient13));
+}
