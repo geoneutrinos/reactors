2bbc5aff6a2fb79973aaa1f268fba8b960c8c3d1
Stephen Dye <47828035+stdye@users.noreply.github.com>
2022-10-19 09:20:32 -1000

    start calculating muon and neutron fluxes

--- src/muons/index.ts
+++ src/muons/index.ts
@@ -1,3 +1,10 @@
 // overburden-dependent muon-induced backgrouds
 
-export {}
+const depthBins = 1000;
+const maximumDepth = 10; km.w.e.
+const deltaDepth = maximumDepth / depthBins; // km.w.e.
+
+// make the array of overburdens 0 - 10 km.w.e.
+export const depthValues = new Float64Array(depthBins).map(
+  (v, i) => i * deltaDepth + deltaDepth / 2
+);
