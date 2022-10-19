b5216b5aefe4ebaf69f5eeec44af915cf50d5367
Stephen Dye <47828035+stdye@users.noreply.github.com>
2022-10-19 10:05:33 -1000

    add muon intensity function

--- src/muons/index.ts
+++ src/muons/index.ts
@@ -8,3 +8,14 @@ const deltaDepth = maximumDepth / depthBins; // km.w.e.
 export const depthValues = new Float64Array(depthBins).map(
   (v, i) => i * deltaDepth + deltaDepth / 2
 );
+
+// Muon Depth Intensity Relationship (DIR) Eq. 1 in Mei and Hime, PRD 73 (2006) 053004
+function muonIntensity(depth: number) {
+  const intensityParameter1 = 8.60e-6; // /cm^2/s/sr
+  const intensityParameter2 = 0.44e-6; // /cm^2/s/sr
+  const overburdenCoefficient1 = 0.45; // km.w.e.
+  const overburdenCoefficient2 = 0.87; // km.w.e.
+    
+  return (intensityParameter1 * Math.exp(-depth / overburdenCoefficient1) + intensityParameter2 * Math.exp(-depth / overburdenCoefficient2));
+}
+  
