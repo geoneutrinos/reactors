20dcee400aa9ee526d9b13d26bb7d9321e5e7f1c
Stephen Dye <47828035+stdye@users.noreply.github.com>
2022-11-13 05:10:22 -1000

    physics-plots: use mathjx for e and p rest masses

--- src/ui/physics-plots.jsx
+++ src/ui/physics-plots.jsx
@@ -1171,10 +1171,13 @@ export const CrossSectionPlots = () => {
           <Node inline>{String.raw`c_\mathrm{R} = c_V - c_A.`}</Node>{" "}
         </p>
         <p>
-          The eES cross section equation given above applies to any two-body elastic scattering process. For neutrino-proton elastic scattering 
-          pES simply replace the electron rest mass <i>m</i><sub>e</sub> with the proton rest mass <i>m</i><sub>p</sub> to account for the heavier 
-          target and use the vector and axial vector coupling factors given for pES in the table below. The pES reaction is an important detection 
-          channel for supernova burst neutrinos (see the SnNu tab).
+          The eES cross section equation given above applies to any two-body elastic scattering process. 
+          For neutrino-proton elastic scattering pES simply replace the electron rest mass{" "}
+          <Node inline>{String.raw`m_\mathrm{e}`}</Node>{" "} 
+          with the proton rest mass{" "}
+          <Node inline>{String.raw`m_\mathrm{p}`}</Node>{" "} 
+          to account for the heavier target and use the vector and axial vector coupling factors given for pES in the table below. 
+          The pES reaction is an important detection channel for supernova burst neutrinos (see the SnNu tab).
         </p>
         <Plot
           useResizeHandler={true}
