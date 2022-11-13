da2a5f3d26dab5c49d6688cfa69a2940d28b4d5e
Stephen Dye <47828035+stdye@users.noreply.github.com>
2022-11-12 16:47:04 -1000

    add pES coupling factor table

--- src/ui/physics-coupling-factors.tsx
+++ src/ui/physics-coupling-factors.tsx
@@ -54,3 +54,56 @@ export const EesCouplingFactors = () => {
     </Card>
   );
 }
+
+export const PesCouplingFactors = () => {
+  return (
+    <Card>
+      <Card.Header>pES Coupling Factors</Card.Header>
+      <Card.Body>
+        <Table>
+            <thead>
+              <tr>
+                <th></th>
+                <th>ν<sub>e</sub></th>
+                <th>ν̅<sub>e</sub></th>
+                <th>ν<sub>x</sub></th>
+                <th>ν̅<sub>x</sub></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td><i>c</i><sub>V</sub></td>
+                <td>
+                  1/2 - 2sin<sup>2</sup><i>θ</i><sub>W</sub>
+                </td>
+                <td>
+                  1/2 - 2sin<sup>2</sup><i>θ</i><sub>W</sub>
+                </td>
+                <td>
+                  1/2 - 2sin<sup>2</sup><i>θ</i><sub>W</sub>
+                </td>
+                <td>
+                  1/2 - 2sin<sup>2</sup><i>θ</i><sub>W</sub>
+                </td>
+              </tr>
+               <tr>
+                <td><i>c</i><sub>A</sub></td>
+                <td>
+                  1.27/2
+                </td>
+                <td>
+                  -1.27/2
+                </td>
+                <td>
+                  1.27/2
+                </td>
+                <td>
+                  -1.27/2
+                </td>
+              </tr>
+            </tbody>
+          </Table>
+      </Card.Body>
+    </Card>
+  );
+}
