8458829779dedd18cc3aa8e209f17c25c4556d58
Stephen Dye <47828035+stdye@users.noreply.github.com>
2022-12-19 07:26:35 -1000

    default location set to Kamioka

--- src/App.js
+++ src/App.js
@@ -106,7 +106,7 @@ L.Icon.Default.mergeOptions({
   shadowUrl: shadowUrl,
 });
 
-const defaultDetector = presets.find((detector) => detector.name === "follow");
+const defaultDetector = presets.find((detector) => detector.name === "Kamioka");
 
 function App(props) {
   const [oscillation, oscillationDispatch] = useReducer(
