25227411244167c93ff2d1fae33f738d7d49e8c0
Stephen Dye <47828035+stdye@users.noreply.github.com>
2023-09-04 16:13:46 -1000

    Update processReactors.js

--- src/reactor-cores/reactor-database/processReactors.js
+++ src/reactor-cores/reactor-database/processReactors.js
@@ -5,7 +5,7 @@ const papaparse = require("papaparse")
 const {parse} = papaparse
 
 const startDate = new Date("2003-01-01Z")
-const endDate = new Date("2021-12-31Z")
+const endDate = new Date("2022-12-31Z")
 
 const processReactorDB = () => {
     let reactor_base = path.join(__dirname, "reactors.csv")
