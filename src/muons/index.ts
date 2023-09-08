3b03d59c5f18cedbf467c4cdf320e5d88dc3d39a
Stephen Dye <47828035+stdye@users.noreply.github.com>
2023-09-07 17:11:25 -1000

    more hacking Update index.ts

--- src/muons/index.ts
+++ src/muons/index.ts
@@ -9,16 +9,6 @@ export const depthValues = new Float64Array(depthBins).map(
   (_v, i) => i * deltaDepth + deltaDepth / 2
 );
 
-// differential muon intensity corresponding to slant depth- Eq. 1 in Mei and Hime, PRD 73 (2006) 053004
-function differentialMuonIntensity(slantDepth: number) {
-  const intensityParameter1a = 8.60e-6; // /cm^2/s/sr
-  const intensityParameter1b = 0.44e-6; // /cm^2/s/sr
-  const overburdenCoefficient1a = 0.45; // km.w.e.
-  const overburdenCoefficient1b = 0.87; // km.w.e.
-    
-  return (intensityParameter1a * Math.exp(-slantDepth / overburdenCoefficient1a) + intensityParameter1b * Math.exp(-slantDepth / overburdenCoefficient1b));
-}
-
 // differential muon intensity corresponding to flat overburden- Eq. 4 in Mei and Hime, PRD 73 (2006) 053004
 export function flatOverburdenMuonIntensity(equivalentDepth: number) {
   const intensityParameter4a = 69.97e-6; // /cm^2/s
@@ -37,6 +27,5 @@ export function neutronInducedFlux(flatDepth: number) {
   return (intensityParameter13 * (overburdenCoefficient13 / flatDepth) * Math.exp(-flatDepth / overburdenCoefficient13));
 }
 
-export const muonSlantIntensity = depthValues.map((Dv) => differentialMuonIntensity(Dv))
 export const muonFlatIntensity = depthValues.map((Dv) => flatOverburdenMuonIntensity(Dv))
 export const neutronFlatIntensity = depthValues.map((Dv) => neutronInducedFlux(Dv))
