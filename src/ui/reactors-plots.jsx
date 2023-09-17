777c0d14786cd4ad5361ec1a7c12f87dd7e4ce1c
Stephen Dye <47828035+stdye@users.noreply.github.com>
2023-09-17 22:20:43 +0100

    specify energy ranges Update reactors-plots.jsx

--- src/ui/reactors-plots.jsx
+++ src/ui/reactors-plots.jsx
@@ -225,19 +225,19 @@ export const FissionIsotopeSpectraPlotsHK = () => {
       <Card.Header>Huber-Kopeikin: Fission Isotope Emission Spectra</Card.Header>
       <Card.Body>
         <p>
-          <sup>239</sup>Pu, <sup>241</sup>Pu conversion spectra (solid):
+          <sup>239</sup>Pu, <sup>241</sup>Pu conversion spectra (solid 2-8 MeV):
           <br />
           P. Huber (2011), <i>Determination of antineutrino spectra from nuclear
           reactors</i>, Phys. Rev. C 84, 024617.
         </p>
         <p>
-          <sup>235</sup>U, <sup>238</sup>U conversion spectra (solid):
+          <sup>235</sup>U, <sup>238</sup>U conversion spectra (solid 2-8 MeV):
           <br />
           V. Kopeikin, M. Skorokhvatov, O. Titov (2021), <i>Reevaluating reactor antineutrino spectra with new measurements of the ratio
           between <sup>235</sup>U and <sup>239</sup>Pu β spectra</i>, Phys. Rev. D 104, L071301.
         </p>
         <p>
-          <sup>235</sup>U, <sup>238</sup>U, <sup>239</sup>Pu, <sup>241</sup>Pu summation spectra (dot-dash):
+          <sup>235</sup>U, <sup>238</sup>U, <sup>239</sup>Pu, <sup>241</sup>Pu summation spectra (dot-dash 0-2, 8-10 MeV):
           <br />
           M. Estienne <i>et al.</i> (2019), <i>Updated Summation Model: An Improved Agreement with the Daya Bay Antineutrino Fluxes</i>,
           Phys. Rev. Lett. 123, 022502.
@@ -470,19 +470,19 @@ export const FissionIsotopeSpectraPlotsHM = () => {
       <Card.Header>Huber-Mueller: Fission Isotope Emission Spectra</Card.Header>
       <Card.Body>
         <p>
-          <sup>235</sup>U, <sup>239</sup>Pu, <sup>241</sup>Pu conversion spectra (solid):
+          <sup>235</sup>U, <sup>239</sup>Pu, <sup>241</sup>Pu conversion spectra (solid 2-8 MeV):
           <br />
           P. Huber (2011), <i>Determination of antineutrino spectra from nuclear
           reactors</i>, Phys. Rev. C 84, 024617.
         </p>
         <p>
-          <sup>238</sup>U <i>ab initio</i> spectrum (solid):
+          <sup>238</sup>U <i>ab initio</i> spectrum (solid 2-8 MeV):
           <br />
           Mueller, Th. A. <i>et al.</i> (2011), <i>Improved predictions of reactor antineutrino
           spectra</i>, Phys. Rev. C 83, 054615.
         </p>
         <p>
-          <sup>235</sup>U, <sup>238</sup>U, <sup>239</sup>Pu, <sup>241</sup>Pu summation spectra (dot-dash):
+          <sup>235</sup>U, <sup>238</sup>U, <sup>239</sup>Pu, <sup>241</sup>Pu summation spectra (dot-dash 0-2, 8-10 MeV):
           <br />
           M. Estienne <i>et al.</i> (2019), <i>Updated Summation Model: An Improved Agreement with the Daya Bay Antineutrino Fluxes</i>,
           Phys. Rev. Lett. 123, 022502.
@@ -588,7 +588,7 @@ export const FissionIsotopeSpectraPlotsES = () => {
       <Card.Header>Estienne et al.: Fission Isotope Emission Spectra</Card.Header>
       <Card.Body>
         <p>
-          <sup>235</sup>U, <sup>238</sup>U, <sup>239</sup>Pu, <sup>241</sup>Pu summation spectra (solid):
+          <sup>235</sup>U, <sup>238</sup>U, <sup>239</sup>Pu, <sup>241</sup>Pu summation spectra (solid 0-10 MeV):
           <br />
           M. Estienne <i>et al.</i> (2019), <i>Updated Summation Model: An Improved Agreement with the Daya Bay Antineutrino Fluxes</i>,
           Phys. Rev. Lett. 123, 022502.
