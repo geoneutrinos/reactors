e7b896a26f3ff34aedf49e0cc22b8007bd49b540
Stephen Dye <47828035+stdye@users.noreply.github.com>
2024-03-24 12:21:42 -1000

    Update geo-prem-plots.jsx

--- src/ui/geo-prem-plots.jsx
+++ src/ui/geo-prem-plots.jsx
@@ -54,7 +54,67 @@ export const GeoDensityPlot = () => {
     };
     return (
       <Card>
-        <Card.Header>PREM</Card.Header>
+        <Card.Header>PREM Density</Card.Header>
+        <Card.Body>
+          <Plot
+            useResizeHandler={true}
+            style={{ width: "100%" }}
+            data={data} 
+            layout={layout}
+            config={config}
+          />
+          <p>This is work in progress with the goal of calculating both the mass and geophysical response of each PREM layer.</p>
+        </Card.Body>
+      </Card>
+    );
+  }
+
+export const GeoMassPlot = () => {
+    const data = [
+      {
+        y: bins.map(bin => rho(bin)),
+        x: bins,
+        name: "Mass",
+        type: "scatter",
+        mode: "lines",
+        fill: "none",
+        marker: { color: "orange" },
+      },
+    ]
+    const layout = {
+      title: "PREM Mass",
+      yaxis: {
+        title: { text: `Mass (g)` },
+        autorange: true
+      },
+      xaxis: {
+        title: { text: `Radius (km)` },
+      },
+      autosize: true,
+      legend: {
+        x: 1,
+        xanchor: "left",
+        y: 1,
+      },
+      annotations: [
+        {
+          showarrow: false,
+          text: "geoneutrinos.org",
+          x: 1.1,
+          xref: "paper",
+          y: -0.15,
+          yref: "paper",
+        },
+      ],
+    };
+    var config = {
+      toImageButtonOptions: {
+        filename: 'PREM_mass'
+      }
+    };
+    return (
+      <Card>
+        <Card.Header>PREM Mass</Card.Header>
         <Card.Body>
           <Plot
             useResizeHandler={true}
