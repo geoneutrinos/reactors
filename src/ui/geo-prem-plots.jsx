eafcd8c1b97eed2d91b5c27f80fead722f893f95
Stephen Dye <47828035+stdye@users.noreply.github.com>
2024-03-24 13:18:14 -1000

    Update geo-prem-plots.jsx

--- src/ui/geo-prem-plots.jsx
+++ src/ui/geo-prem-plots.jsx
@@ -72,7 +72,7 @@ export const GeoDensityPlot = () => {
 export const GeoMassPlot = () => {
     const data = [
       {
-        y: bins.map(bin => rho(bin)),
+        y: bins.map(bin => rho(bin) * preFactor),
         x: bins,
         name: "Mass",
         type: "scatter",
