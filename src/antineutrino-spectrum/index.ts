ec2958edbd77ec92e98c8e921215806346e9e5ee
Stephen Dye <47828035+stdye@users.noreply.github.com>
2024-04-11 15:01:01 -1000

    Update index.ts

--- src/antineutrino-spectrum/index.ts
+++ src/antineutrino-spectrum/index.ts
@@ -46,7 +46,7 @@ function resample(
   });
 }
 
-const plotAntineutrinoSpectrum40K = antineutrinoSpectrum40KData.map((x) => x * 1000 * K40_BRANCH_RATIO.beta);
+export const plotAntineutrinoSpectrum40K = antineutrinoSpectrum40KData.map((x) => x * 1000 * K40_BRANCH_RATIO.beta);
 
 export const rawAntineutrinoSpectrum = {
   "40K": antineutrinoSpectrum40KData,
