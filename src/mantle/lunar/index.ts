a69ee63067a8c82caf775492f4e97a0dc4b3733a
Andrew Barna <abarna@gmail.com>
2024-05-31 03:41:23 +0000

    Fix using the wrong density for the lunar lvz

--- src/mantle/lunar/index.ts
+++ src/mantle/lunar/index.ts
@@ -34,7 +34,7 @@ function geoIntegrate(inner: number, outer: number): number {
 
 // it looks like we only need two layers from this model, the LVZ and the mantle
 // The core(s) are assumed to not contribute and the cruse we will get from the seperately modeled crust flux
-export const lvzMass = briaudEtAl2023[3][1] * shellVolume(362, 560)
+export const lvzMass = briaudEtAl2023[2][1] * shellVolume(362, 560)
 export const lvzGeophysicalResponse = geoIntegrate(362, 560) * briaudEtAl2023[2][1] * maxRadius * 100 / 2
 export const mantleMass = briaudEtAl2023[3][1] * shellVolume(560, 1698.6)
 export const mantleGeophysicalResponse = geoIntegrate(560, 1698.6) * briaudEtAl2023[3][1] * maxRadius * 100 / 2
