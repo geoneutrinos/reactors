aac0ebbcb56f0464e2803005d59f26fedbc44354
Stephen Dye <47828035+stdye@users.noreply.github.com>
2024-05-31 17:29:14 -1000

    Update lunar-input-plots.tsx

--- src/ui/lunar-input-plots.tsx
+++ src/ui/lunar-input-plots.tsx
@@ -138,7 +138,7 @@ export const Lunar238UFlux = memo(() => {
     },
   ];
   const layout = {
-    title: "Lunar 238U Flux (/cm<sup>2</sup>/us)",
+    title: "Lunar Crust <sup>238</sup>U Flux (/cm<sup>2</sup>/us)",
     yaxis: {
       title: { text: `Latitude` },
       autorange: true,
@@ -197,7 +197,7 @@ export const Lunar232ThFlux = memo(() => {
     },
   ];
   const layout = {
-    title: "Lunar 232Th Flux (/cm<sup>2</sup>/us)",
+    title: "Lunar Crust <sup>232</sup>Th Flux (/cm<sup>2</sup>/us)",
     yaxis: {
       title: { text: `Latitude` },
       autorange: true,
@@ -256,7 +256,7 @@ export const Lunar40KFlux = memo(() => {
     },
   ];
   const layout = {
-    title: "Lunar 40K Flux (/cm<sup>2</sup>/us)",
+    title: "Lunar Crust <sup>40</sup>K<sub>β</sub> Flux (/cm<sup>2</sup>/us)",
     yaxis: {
       title: { text: `Latitude` },
       autorange: true,
