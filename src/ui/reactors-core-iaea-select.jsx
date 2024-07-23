227a9dc77ec5f8840a24c637d9494d79850c4202
Stephen Dye <47828035+stdye@users.noreply.github.com>
2024-07-23 11:54:38 -1000

    Update reactors-core-iaea-select.jsx

--- src/ui/reactors-core-iaea-select.jsx
+++ src/ui/reactors-core-iaea-select.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { range } from "lodash";
 import { Form, Card } from "react-bootstrap";
 
-const years = range(2003, 2023);
+const years = range(2003, 2024);
 const months = range(1, 13);
 
 export const CoreIAEARange = ({
