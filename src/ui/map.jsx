2c10e53766cfe2c2fe426d53b383a0adc1e230a4
Stephen Dye <47828035+stdye@users.noreply.github.com>
2025-09-14 16:15:55 -1000

    change "Detector Locations" to "Operating Detectors"

--- src/ui/map.jsx
+++ src/ui/map.jsx
@@ -248,7 +248,7 @@ export function NuMap({
             <CoreCircles cores={cores} zoom={zoom} shutdownCores={false}/>
           </LayerGroup>
         </LayersControl.Overlay>
-        <LayersControl.Overlay checked name="Detector Sites">
+        <LayersControl.Overlay checked name="Operating Detectors">
           <LayerGroup>
             <DetectorCircles
               detectors={detectorList}
