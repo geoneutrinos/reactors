3b27ed8db3f7f915376b8415357511b99dacdb66
Stephen Dye <47828035+stdye@users.noreply.github.com>
2025-10-07 17:58:29 -1000

    change detector circle color Update map.jsx

--- src/ui/map.jsx
+++ src/ui/map.jsx
@@ -29,7 +29,8 @@ const DetectorCircles = React.memo(function DetectorCircles({
   celestialBody,
 }) {
   detectors = detectors.filter(detector => REGIONS[celestialBody].has(detector.region))
-  const color = "#9d00ff";
+//  const color = "#9d00ff";
+  const color = "#ffff00";
   const radius = zoom > 6? zoom > 8? zoom > 10? zoom > 12? zoom > 14? zoom > 18? 25 : 50 : 500 : 1000 : 2000 : 5000 : 10000
   return detectors.map((detector) => {
     const DetectorPopup = (
