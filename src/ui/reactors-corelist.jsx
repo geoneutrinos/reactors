58f1050007a1065c40651a2a5d53a9e4f0dd7175
Stephen Dye <47828035+stdye@users.noreply.github.com>
2026-01-10 14:10:16 -1000

    add core.power to output Update reactors-corelist.jsx

--- src/ui/reactors-corelist.jsx
+++ src/ui/reactors-corelist.jsx
@@ -304,7 +304,7 @@ export const CoreList = ({
       </Card.Header>
       <Card.Body>
         <p> Filter Cores by Name or Type (PWR, BWR, PHWR, GCR, LWGR, FBR, LEU_MOX) </p>
-        <DownloadButton data={Object.values(cores)} cols={["name", "detectorDistance", "detectorNIU", "direction.phi", "direction.elev"]} buttonTitle="Download Data" filename="cores.csv"/>
+        <DownloadButton data={Object.values(cores)} cols={["name", "detectorDistance", "power", "detectorNIU", "direction.phi", "direction.elev"]} buttonTitle="Download Data" filename="cores.csv"/>
       </Card.Body>
       <ListGroup variant="flush">
         <ListGroup.Item>
