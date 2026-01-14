0ce60926a1f1448cd5d6ad706c245499abbb9733
Stephen Dye <47828035+stdye@users.noreply.github.com>
2026-01-14 12:54:58 -1000

    misplaced comma reactors-corelist.jsx

--- src/ui/reactors-corelist.jsx
+++ src/ui/reactors-corelist.jsx
@@ -144,7 +144,7 @@ const CoreListItem = ({
           Elevation: {core.elevation} m
           <br />
           Distance: {dist} km< br/>
-          Azim: {ENUtoNEU(core.direction.phi.toFixed(1))}&deg; Alt: {core.direction.elev.toFixed(1)}&deg;
+          Azim: {ENUtoNEU(core.direction.phi).toFixed(1)}&deg; Alt: {core.direction.elev.toFixed(1)}&deg;
         </Col>
       </Row>
     </ListGroup.Item>
