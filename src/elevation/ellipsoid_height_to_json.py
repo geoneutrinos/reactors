2e55030a7bfecf85686e98d04ebfb0693e46ff3b
Stephen Dye <47828035+stdye@users.noreply.github.com>
2026-02-02 12:47:58 -1000

    Update ellipsoid_height_to_json.py

--- src/elevation/ellipsoid_height_to_json.py
+++ src/elevation/ellipsoid_height_to_json.py
@@ -9,9 +9,9 @@ raw_data = pyexcel.get_array(file_name="Earth_Ellipsoid_Height.xlsx")
 
 
 data = {
-    "o": [],
-    "g": [],
-    "e": [],
+    "ortho": [],
+    "geoid": [],
+    "ellip": [],
 }
 
 O_COL = 2
@@ -24,17 +24,17 @@ for row in raw_data[2:]:
         # This should be the end of the data, to just stop
         break
     # precision in the excel is 2, so keep that
-    data["o"].append(round(float(row[O_COL]), 2))
-    data["g"].append(round(float(row[G_COL]), 2))
-    data["e"].append(round(float(row[E_COL]), 2))
+    data["ortho"].append(round(float(row[O_COL]), 2))
+    data["geoid"].append(round(float(row[G_COL]), 2))
+    data["ellip"].append(round(float(row[E_COL]), 2))
 
 # the input data is on a 1x1 degree grid of lon lats
 expected_array_length = 360 * 180
-assert(len(data["o"]) == expected_array_length)
-assert(len(data["g"]) == expected_array_length)
-assert(len(data["e"]) == expected_array_length)
+assert(len(data["ortho"]) == expected_array_length)
+assert(len(data["geoid"]) == expected_array_length)
+assert(len(data["ellip"]) == expected_array_length)
 
 json.encoder.FLOAT_REPR = lambda f: ("%.3f" % f)
 for element, flux in data.items():
-    with open(f"crust_{element}.json", "w") as f:
+    with open(f"elevation_{element}.json", "w") as f:
         json.dump(flux, f, separators=(',', ':'))
