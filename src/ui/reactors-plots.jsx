5a6fc071cd93253fe5527cc33f6f4e41e53ab083
Stephen Dye <47828035+stdye@users.noreply.github.com>
2026-02-17 14:53:20 -1000

    remove spaces

--- src/ui/reactors-plots.jsx
+++ src/ui/reactors-plots.jsx
@@ -707,8 +707,7 @@ export const CoreDirectionSignalPlots = ({ cores, detector, reactorLF }) => {
         : detector.current
     } to ${first.name}<br /><sub>(${reactorLF.start.toISOString().slice(0, 7)} through ${reactorLF.end.toISOString().slice(0, 7)} avg Load Factor)</sub>`,
     yaxis: {
-      title: { text: `Reactor Rate (NIU)<br /><sub>(${crossSection.elasticScatteringTMin.toFixed(2)} 
-        &lt; T<sub>e</sub> &lt; ${crossSection.elasticScatteringTMax.toFixed(2)} MeV)</sub>`},
+      title: { text: `Reactor Rate (NIU)<br /><sub>(${crossSection.elasticScatteringTMin.toFixed(2)} &lt; T<sub>e</sub> &lt; ${crossSection.elasticScatteringTMax.toFixed(2)} MeV)</sub>`},
       type: 'log',
       autorange: true
     },
